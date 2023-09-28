75cf96198e2391983438f3dc4384da9e4c6d4d0a
Ingrid <git@ingrids.email>
2023-09-28 17:57:12 +0200

    add prototype schema

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE public.data (
+    timeseries INT8 REFERENCES public.timeseries NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    value REAL,
+    PRIMARY KEY (timeseries, timestamp)
+) PARTITION BY RANGE (timestamp);
+
+CREATE TYPE location AS (
+    lat REAL,
+    lon REAL,
+    hamsl REAL,
+    hag REAL,
+);
+
+CREATE TABLE public.timeseries (
+    id BIGSERIAL PRIMARY KEY,
+    fromtime TIMESTAMPTZ NULL,
+    totime TIMESTAMPTZ NULL,
+    loc location NULL, 
+    updatedat TIMESTAMPTZ NOT NULL DEFAULT now() :: TIMESTAMPTZ,
+    deactivated BOOL NULL
+);
+CREATE INDEX updatedat_timeseries_index ON timeseries (updatedat ASC);
+
+/*
+    TODO: 
+    - Consider what to do with labels
+      probably do a labels namespace with a table per labeltype
+    - Figure out products
+    - Figure out datagroups
+    - Figure out RPC
+    - Revisit purpose of timeseries.updatedat and timeseries.deactivated
+*/
