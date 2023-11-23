e19e523f12808c3430793cd807e6660c05cec0bb
Ingrid <git@ingrids.email>
2023-11-23 16:47:14 +0100

    api: add support for regular timeseries

--- api/src/timeseries.rs
+++ api/src/timeseries.rs
@@ -23,6 +23,21 @@ pub struct TimeseriesIrregular {
     timestamps: Vec<DateTime<Utc>>,
 }
 
+#[derive(Debug, Serialize)]
+pub struct TimeseriesRegular {
+    header: TimeseriesInfo,
+    data: Vec<Option<f32>>,
+    start_time: DateTime<Utc>,
+    time_resolution: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "regularity")]
+pub enum Timeseries {
+    Regular(TimeseriesRegular),
+    Irregular(TimeseriesIrregular),
+}
+
 pub async fn get_timeseries_info(
     conn: &PooledPgConn<'_>,
     station_id: i32,
@@ -32,7 +47,7 @@ pub async fn get_timeseries_info(
         .query_one(
             "SELECT timeseries.id, \
                 COALESCE(timeseries.fromtime, '1950-01-01 00:00:00+00'), \
-                COALESCE(timeseries.totime, '9999-01-01 00:00:00+00'), \
+                COALESCE(timeseries.totime, NOW()::timestamptz), \
                 filter.lvl, \
                 filter.sensor, \
                 timeseries.loc \
@@ -46,6 +61,7 @@ pub async fn get_timeseries_info(
 
     let ts_id: i32 = ts_result.get(0);
     let fromtime: DateTime<Utc> = ts_result.get(1);
+    // TODO: there might be a better way to deal with totime than that COALESCE
     let totime: DateTime<Utc> = ts_result.get(2);
 
     Ok(TimeseriesInfo {
@@ -75,7 +91,7 @@ pub async fn get_timeseries_data_irregular(
         )
         .await?;
 
-    let ts_irregular = {
+    let ts = {
         let mut data = Vec::with_capacity(data_results.len());
         let mut timestamps = Vec::with_capacity(data_results.len());
 
@@ -91,5 +107,52 @@ pub async fn get_timeseries_data_irregular(
         }
     };
 
-    Ok(ts_irregular)
+    Ok(ts)
+}
+
+pub async fn get_timeseries_data_regular(
+    conn: &PooledPgConn<'_>,
+    header: TimeseriesInfo,
+    start_time: DateTime<Utc>,
+    end_time: DateTime<Utc>,
+    time_resolution: String,
+) -> Result<TimeseriesRegular, tokio_postgres::Error> {
+    // TODO: string matching intervals like this is a hack, but currently necessary to avoid
+    // SQL injection. Ideally we could pass an interval type as a query param, which would
+    // also save us the query_string allocation, but no ToSql implementations for intervals
+    // currently exist in tokio_postgres, so we need to implement it ourselves.
+    let interval = match time_resolution.as_str() {
+        "PT1M" => "1 minute",
+        "PT1H" => "1 hour",
+        "P1D" => "1 day",
+        _ => "1 minute", // FIXME: this should error instead of falling back to a default
+    };
+    let query_string = format!("SELECT data.obsvalue, ts_rule.timestamp \
+                FROM (SELECT data.obsvalue, data.obstime FROM data WHERE data.timeseries = $1) as data 
+                    RIGHT JOIN generate_series($2::timestamptz, $3::timestamptz, interval '{}') AS ts_rule(timestamp) \
+                        ON data.obstime = ts_rule.timestamp", interval);
+
+    let data_results = conn
+        .query(
+            query_string.as_str(),
+            &[&header.ts_id, &start_time, &end_time],
+        )
+        .await?;
+
+    let ts = {
+        let mut data = Vec::with_capacity(data_results.len());
+
+        for row in data_results {
+            data.push(row.get(0));
+        }
+
+        TimeseriesRegular {
+            header,
+            data,
+            start_time,
+            time_resolution,
+        }
+    };
+
+    Ok(ts)
 }
