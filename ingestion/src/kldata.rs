65235fc9eb26cf94c06be00f048259aa62fdfaab
Ingrid <git@ingrids.email>
2024-01-22 21:03:24 +0100

    ingestion/kldata: parse csv body

--- ingestion/src/kldata.rs
+++ ingestion/src/kldata.rs
@@ -1,19 +1,22 @@
+use chrono::{DateTime, Utc};
 use regex::Regex;
 use std::str::Lines;
 
-// TODO: this should go somewhere more shared
 // TODO: verify integer types
-pub struct Obs {
-    obstime: i32,
-    id: i32,
+pub struct ObsinnObs {
+    timestamp: DateTime<Utc>,
+    id: ObsinnId,
+    value: f32,
 }
 
-struct KlMeta {
+pub struct ObsinnChunk {
+    observations: Vec<ObsinnObs>,
     station_id: i32,
     type_id: i32,
 }
 
-struct Column {
+#[derive(Debug, Clone)]
+struct ObsinnId {
     param_code: String,
     sensor_and_level: Option<(i32, i32)>,
 }
@@ -29,7 +32,7 @@ fn parse_meta_field(
     Ok(value.parse().unwrap())
 }
 
-fn parse_meta(meta: &str) -> Result<KlMeta, &dyn std::error::Error> {
+fn parse_meta(meta: &str) -> Result<(i32, i32), &dyn std::error::Error> {
     let mut parts = meta.splitn(4, '/');
 
     assert_eq!(parts.next().unwrap(), "kldata");
@@ -37,13 +40,10 @@ fn parse_meta(meta: &str) -> Result<KlMeta, &dyn std::error::Error> {
     let station_id = parse_meta_field(parts.next().unwrap(), "station_id").unwrap();
     let type_id = parse_meta_field(parts.next().unwrap(), "type_id").unwrap();
 
-    Ok(KlMeta {
-        station_id,
-        type_id,
-    })
+    Ok((station_id, type_id))
 }
 
-fn parse_columns(cols_raw: &str) -> Result<Vec<Column>, &dyn std::error::Error> {
+fn parse_columns(cols_raw: &str) -> Result<Vec<ObsinnId>, &dyn std::error::Error> {
     // this regex is taken from kvkafka's kldata parser
     // let col_regex = Regex::new(r"([^(),]+)(\([0-9]+,[0-9]+\))?").unwrap();
     // it is modified below to capture sensor and level separately, while keeping
@@ -53,11 +53,11 @@ fn parse_columns(cols_raw: &str) -> Result<Vec<Column>, &dyn std::error::Error> 
     Ok(col_regex
         .captures_iter(cols_raw)
         .map(|caps| match caps.len() {
-            2 => Column {
+            2 => ObsinnId {
                 param_code: caps.get(1).unwrap().as_str().to_owned(),
                 sensor_and_level: None,
             },
-            4 => Column {
+            4 => ObsinnId {
                 param_code: caps.get(1).unwrap().as_str().to_owned(),
                 sensor_and_level: Some((
                     caps.get(2).unwrap().as_str().parse().unwrap(),
@@ -71,22 +71,52 @@ fn parse_columns(cols_raw: &str) -> Result<Vec<Column>, &dyn std::error::Error> 
 
 fn parse_obs(
     csv_body: Lines<'_>,
-    columns: &[Column],
-) -> Result<Vec<Obs>, Box<dyn std::error::Error>> {
-    todo!()
+    columns: &[ObsinnId],
+) -> Result<Vec<ObsinnObs>, Box<dyn std::error::Error>> {
+    let mut obs = Vec::new();
+
+    for row in csv_body {
+        let (timestamp, vals) = {
+            let mut vals = row.split(',');
+            // TODO: timestamp parsing needs to handle milliseconds and truncated timestamps
+            let timestamp = DateTime::parse_from_str(vals.next().unwrap(), "%Y%m%d%H%M%S")
+                .unwrap()
+                .with_timezone(&Utc);
+
+            (timestamp, vals)
+        };
+
+        for (i, val) in vals.enumerate() {
+            let col = columns[i].clone(); // Should we do some smart bounds-checking??
+
+            // TODO: parse differently based on param_code?
+
+            obs.push(ObsinnObs {
+                timestamp,
+                id: col,
+                value: val.parse().unwrap(),
+            })
+        }
+    }
+
+    Ok(obs)
 }
 
-pub fn parse_kldata(msg: &str) -> Result<Vec<Obs>, &dyn std::error::Error> {
+pub fn parse_kldata(msg: &str) -> Result<ObsinnChunk, &dyn std::error::Error> {
     // parse the first two lines of the message as meta, and csv column names,
     // leave the rest as an iter over the lines of csv body
-    let (meta, columns, csv_body) = {
+    let (station_id, type_id, columns, csv_body) = {
         let mut lines = msg.lines();
 
-        let meta = parse_meta(lines.next().unwrap()).unwrap();
+        let (station_id, type_id) = parse_meta(lines.next().unwrap()).unwrap();
         let columns = parse_columns(lines.next().unwrap()).unwrap();
 
-        (meta, columns, lines)
+        (station_id, type_id, columns, lines)
     };
 
-    Ok(parse_obs(csv_body, &columns).unwrap())
+    Ok(ObsinnChunk {
+        observations: parse_obs(csv_body, &columns).unwrap(),
+        station_id,
+        type_id,
+    })
 }
