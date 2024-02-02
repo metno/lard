79fdf7c5f8304ea5fd0956160e720da0e78a9981
Ingrid <git@ingrids.email>
2024-02-02 21:54:55 +0100

    ingestion: setup db conn pool

--- ingestion/src/main.rs
+++ ingestion/src/main.rs
@@ -1,6 +1,10 @@
-use axum::{response::Json, routing::post, Router};
+use axum::{extract::State, response::Json, routing::post, Router};
+use bb8_postgres::PostgresConnectionManager;
 use chrono::{DateTime, Utc};
 use serde::Serialize;
+use tokio_postgres::NoTls;
+
+type PgConnectionPool = bb8::Pool<PostgresConnectionManager<NoTls>>;
 
 pub struct Datum {
     timeseries_id: i32,
@@ -9,7 +13,7 @@ pub struct Datum {
 }
 
 pub mod kldata;
-use kldata::parse_kldata;
+use kldata::{label_kldata, parse_kldata};
 
 #[derive(Debug, Serialize)]
 struct KldataResp {
@@ -19,11 +23,12 @@ struct KldataResp {
     retry: bool,
 }
 
-async fn handle_kldata(body: String) -> Json<KldataResp> {
-    let (message_id, _obsinn_chunk) = parse_kldata(&body).unwrap();
+async fn handle_kldata(State(pool): State<PgConnectionPool>, body: String) -> Json<KldataResp> {
+    let mut conn = pool.get().await.unwrap();
 
-    // TODO: Find or generate obsinn labels
-    // TODO: Find or generate filter labels
+    let (message_id, obsinn_chunk) = parse_kldata(&body).unwrap();
+
+    let data = label_kldata(obsinn_chunk, &mut conn);
 
     // TODO: Insert into data table
 
@@ -38,8 +43,27 @@ async fn handle_kldata(body: String) -> Json<KldataResp> {
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // TODO: use clap for argument parsing
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 4 {
+        panic!("not enough args passed in, at least host, user, dbname needed, optionally password")
+    }
+
+    let mut connect_string = format!("host={} user={} dbname={}", &args[1], &args[2], &args[3]);
+    if args.len() > 4 {
+        connect_string.push_str(" password=");
+        connect_string.push_str(&args[4])
+    }
+
+    // set up postgres connection pool
+    let manager = PostgresConnectionManager::new_from_stringlike(connect_string, NoTls).unwrap();
+    let pool = bb8::Pool::builder().build(manager).await.unwrap();
+
     // build our application with a single route
-    let app = Router::new().route("/kldata", post(handle_kldata));
+    let app = Router::new()
+        .route("/kldata", post(handle_kldata))
+        .with_state(pool);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
