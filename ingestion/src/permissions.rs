042f47645001ba6d119cfdf771575f1b7cf4c926
Ingrid <git@ingrids.email>
2024-03-12 17:56:49 +0100

    ingestion: refine permissions logic based on Pim's feedback

--- ingestion/src/permissions.rs
+++ ingestion/src/permissions.rs
@@ -1,4 +1,5 @@
 use crate::Error;
+use chrono::{DateTime, Utc};
 use std::{
     collections::HashMap,
     sync::{Arc, RwLock},
@@ -11,6 +12,8 @@ pub struct Permit {
     param_id: i32,
     level: i32,
     sensor: i32,
+    from_time: DateTime<Utc>, // make sure UTC is correct for this
+    to_time: Option<DateTime<Utc>>,   // make sure UTC is correct for this
 }
 
 type StationId = i32;
@@ -32,16 +35,10 @@ pub async fn fetch_open_permits() -> Result<PermitTable, Error> {
     // query permit table
     let rows = client
         .query(
-            "SELECT stationid, message_formatid, paramid, hlevel, sensor \
+            "SELECT stationid, message_formatid, paramid, hlevel, sensor, fromtime, totime \
                 FROM v_message_policy \
                 /* we're only interested in open data, which is signalled by permitid = 1 */
-                WHERE permitid = 1  \
-                    /* if there is a totime, the open permit will stop applying at some point, so we
-                       shouldn't ingest since we have currently have no mechanism to remove the data when 
-                       the permit expires */
-                    AND totime IS NULL \
-                    /* if fromtime is in the future, the open permit does not apply now */
-                    AND (fromtime IS NULL OR fromtime < NOW())",
+                WHERE permitid = 1",
             &[],
         )
         .await?;
@@ -49,6 +46,9 @@ pub async fn fetch_open_permits() -> Result<PermitTable, Error> {
     // build hashmap
     let mut open_permits = HashMap::new();
 
+    // indexing rows by number index instead of column name may be more performant
+    // I'm not sure if this optimisation is worth the readability tradeoff though, especially
+    // since the table if so small and this refresh is a background task
     for row in rows {
         open_permits
             .entry(row.get("stationid"))
@@ -58,6 +58,8 @@ pub async fn fetch_open_permits() -> Result<PermitTable, Error> {
                 param_id: row.get("paramid"),
                 level: row.get("hlevel"),
                 sensor: row.get("sensor"),
+                from_time: row.get("fromtime"),
+                to_time: row.get("totime"),
             });
     }
 
@@ -70,18 +72,26 @@ pub fn timeseries_is_open(
     type_id: i32,
     param_id: i32,
     sensor_and_level: Option<(i32, i32)>,
+    timestamp: DateTime<Utc>,
 ) -> Result<bool, Error> {
     if let Some(station_permits) = open_permits
         .read()
         .map_err(|e| Error::Lock(e.to_string()))?
         .get(&station_id)
     {
+        // unwrapping these as 0 is fine because they're only checked if the values in the permit
+        // are not zero. we could keep them as option, but it would make the boolean logic below
+        // gnarlier 
         let (sensor, level) = sensor_and_level.unwrap_or((0, 0));
 
         // if any of the permits fit, return true
         return Ok(station_permits.iter().any(|permit| {
             // permit must apply to all type ids or this specific one
-            (permit.type_id == 0 || permit.type_id == type_id)
+            (permit.type_id == 0 || permit.type_id == type_id) 
+                // from_time must be in the past
+                && (permit.from_time < timestamp)
+                // to_time if exists, must be in the future
+                && (permit.to_time.is_none() || (permit.to_time.unwrap() < timestamp))
                 // param id 0 means permit covers all params, levels, and sensors
                 && (permit.param_id == 0
                     || (permit.param_id == param_id
