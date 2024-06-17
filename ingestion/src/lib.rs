f36a9ecc43e2787d9d7d6398c1553a5300b09c96
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-06-17 14:07:41 +0200

    Add ingestion integration tests

--- ingestion/src/lib.rs
+++ ingestion/src/lib.rs
@@ -86,6 +86,18 @@ pub struct Datum {
     value: f32,
 }
 
+// Only used in tests
+#[doc(hidden)]
+impl Datum {
+    pub fn new(timeseries_id: i32, timestamp: DateTime<Utc>, value: f32) -> Self {
+        Datum {
+            timeseries_id,
+            timestamp,
+            value,
+        }
+    }
+}
+
 pub type Data = Vec<Datum>;
 
 pub async fn insert_data(data: Data, conn: &mut PooledPgConn<'_>) -> Result<(), Error> {
