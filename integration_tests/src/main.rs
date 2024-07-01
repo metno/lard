a4468c6b4d4ff8a5ea156a242d008949524d97d3
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-07-01 11:04:00 +0200

    Clean up main.rs

--- integration_tests/src/main.rs
+++ integration_tests/src/main.rs
@@ -4,28 +4,40 @@ use tokio_postgres::{Error, NoTls};
 
 const CONNECT_STRING: &str = "host=localhost user=postgres dbname=postgres password=postgres";
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let (client, connection) = tokio_postgres::connect(CONNECT_STRING, NoTls).await?;
+async fn insert_schema(client: &tokio_postgres::Client, filename: &str) -> Result<(), Error> {
+    let schema = fs::read_to_string(filename).expect("Should be able to read SQL file");
+    client.batch_execute(schema.as_str()).await
+}
 
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
-
-    let public_schema =
-        fs::read_to_string("db/public.sql").expect("Should be able to read SQL file");
-    client.batch_execute(public_schema.as_str()).await?;
-
-    let labels_schema =
-        fs::read_to_string("db/labels.sql").expect("Should be able to read SQL file");
-    client.batch_execute(labels_schema.as_str()).await?;
-
+async fn create_data_partition(client: &tokio_postgres::Client) -> Result<(), Error> {
     // TODO: add multiple partitions?
     let partition_string = format!(
         "CREATE TABLE data_y{}_to_y{} PARTITION OF public.data FOR VALUES FROM ('{}') TO ('{}')",
         "1950", "2100", "1950-01-01 00:00:00+00", "2100-01-01 00:00:00+00",
     );
     client.batch_execute(partition_string.as_str()).await
+}
+
+#[tokio::main]
+async fn main() {
+    let (client, connection) = tokio_postgres::connect(CONNECT_STRING, NoTls)
+        .await
+        .expect("Should be able to connect to database");
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {e}");
+        }
+    });
+
+    let schemas = ["db/public.sql", "db/labels.sql"];
+    for schema in schemas {
+        if let Err(e) = insert_schema(&client, schema).await {
+            eprintln!("Error: {e} ({schema})")
+        }
+    }
+
+    if let Err(e) = create_data_partition(&client).await {
+        eprintln!("connection error: {e}");
+    }
 }
