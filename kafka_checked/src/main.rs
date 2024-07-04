5fbc6710c31c48c22b25b37bf2c9c169bde161e8
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-07-04 13:54:33 +0200

    Fix small inconsistency

--- kafka_checked/src/main.rs
+++ kafka_checked/src/main.rs
@@ -109,13 +109,13 @@ struct Kvdata {
     cfailed: Option<i32>,
 }
 
-fn optional<'de, D, T>(deserializer: D) -> Result<Option<T>, D::Error>
+fn optional<'de, D, T>(des: D) -> Result<Option<T>, D::Error>
 where
     D: Deserializer<'de>,
     T: std::str::FromStr,
     <T as std::str::FromStr>::Err: std::fmt::Debug,
 {
-    Option::deserialize(deserializer).map(|opt| match opt {
+    Option::deserialize(des).map(|opt| match opt {
         Some("") | None => None,
         Some(val) => Some(val.parse::<T>().unwrap()),
     })
