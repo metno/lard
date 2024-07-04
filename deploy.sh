1bff4704a42fade271131831300cc3c4e46f209c
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-07-04 12:07:06 +0200

    Fix ansible errors

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ fi
 pushd ansible || exit
 
 cp ../target/release/lard_ingestion roles/deploy/files/.
-cp ../ingestion/resources roles/deploy/files/.
+cp -r ../ingestion/resources roles/deploy/files/.
 
 ansible-playbook -i inventory.yml deploy.yml
 
