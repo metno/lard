650598573ac9dda07586c63ad0df47be3a7dd702
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-07-12 16:03:57 +0200

    Change ansible envars service setup

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ pushd ansible || exit
 cp ../target/release/lard_ingestion roles/deploy/files/.
 cp -r ../ingestion/resources roles/deploy/files/.
 
-ansible-playbook -i inventory.yml deploy.yml
+ansible-playbook -i inventory.yml deploy.yml --ask-vault-pass
 
 popd || exit
