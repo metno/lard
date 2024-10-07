635cd9b080fae5ff3962f57296d057119bae8e45
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-10-07 14:14:01 +0200

    Update deploy process and add ansible migration script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 if ! cargo build --workspace --release; then
-  exit 1
+    exit 1
 fi
 
 pushd ansible || exit
-
-cp ../target/release/lard_ingestion roles/deploy/files/.
-cp -r ../ingestion/resources roles/deploy/files/.
 
 ansible-playbook -i inventory.yml deploy.yml --ask-vault-pass
 
