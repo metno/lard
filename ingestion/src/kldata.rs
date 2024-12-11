080ffc491a9fdceb14c7eb3a6dfb8272b829978f
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-12-11 12:54:11 +0100

    Parse special metar params

--- ingestion/src/kldata.rs
+++ ingestion/src/kldata.rs
@@ -40,6 +40,7 @@ pub enum ParseError {
 
 // List of non scalar param codes we don't need to log since we already know their type
 const EXCLUDE_TEXT_LOG: [&str; 2] = ["KLOBS", "signature"];
+const SPECIAL_METAR_CASES: [&str; 5] = ["X1R", "X2R", "X3R", "WS", "WS2"];
 
 /// Represents a set of observations that came in the same message from obsinn, with shared
 /// station_id and type_id
@@ -188,9 +189,10 @@ fn parse_obs(
             let value = match reference_params.get(&col.param_code) {
                 Some(ref_param) => {
                     // NOTE: we assume ref_params marked as scalar in Stinfosys to be floats (but
-                    // could be ints, which wouldn't be ideal)
+                    // could be ints, which wouldn't be ideal?)
                     if ref_param.is_scalar {
                         num_scalar += 1;
+                        // TODO: move to separate function
                         // NOTE(1): some params can be empty (old formats that were carried over
                         // or a hacky way to have the observations deleted)
                         // NOTE(2): some params can be simply "-" instead of being empty (hack?
@@ -198,12 +200,20 @@ fn parse_obs(
                         if val.is_empty() || val == "-" {
                             ObsType::Scalar(None)
                         } else {
-                            // TODO: should we simply return ObsType::Scalar(None) instead?
-                            let parsed = val
-                                .parse()
-                                .map_err(|_| ParseError::Float(val.to_string()))?;
+                            // FIXME: these params are scalar in Stinfosys but come in as 'xxL' and
+                            // 'xxR', where 'x' is a numeric character.
+                            // We need to decide how to treat them (Kvalobs silently discards them apparently)
+                            // Or to change them in Stinfosys?
+                            if SPECIAL_METAR_CASES.contains(&col.param_code.as_str()) {
+                                ObsType::NonScalar(val)
+                            } else {
+                                // TODO: should we simply return ObsType::Scalar(None) instead?
+                                let parsed = val
+                                    .parse()
+                                    .map_err(|_| ParseError::Float(val.to_string()))?;
 
-                            ObsType::Scalar(Some(parsed))
+                                ObsType::Scalar(Some(parsed))
+                            }
                         }
                     } else {
                         num_nonscalar += 1;
@@ -811,7 +821,7 @@ mod tests {
                 "unrecognised param code",
             ),
             (
-                "20240910000000,-0.50,,0.70,,-",
+                "20240910000000,-0.50,,0.70,",
                 vec![
                     ObsinnId {
                         param_code: "TA".to_string(),
@@ -828,10 +838,6 @@ mod tests {
                     ObsinnId {
                         param_code: "FGN_01".to_string(),
                         sensor_and_level: None,
-                    },
-                    ObsinnId {
-                        param_code: "TJ".to_string(),
-                        sensor_and_level: Some((0, 3000)),
                     },
                 ],
                 ObsinnHeader {
@@ -870,6 +876,37 @@ mod tests {
                             },
                             value: NonScalar(""),
                         },
+                    ],
+                    timestamp: Utc.with_ymd_and_hms(2024, 9, 10, 0, 0, 0).unwrap(),
+                    station_id: 18700,
+                    type_id: 511,
+                }]),
+                "parameter with missing observations",
+            ),
+            (
+                "20240910000000,-,24R,24L",
+                vec![
+                    ObsinnId {
+                        param_code: "TJ".to_string(),
+                        sensor_and_level: Some((0, 3000)),
+                    },
+                    ObsinnId {
+                        param_code: "X1R".to_string(),
+                        sensor_and_level: None,
+                    },
+                    ObsinnId {
+                        param_code: "X2R".to_string(),
+                        sensor_and_level: None,
+                    },
+                ],
+                ObsinnHeader {
+                    station_id: 18700,
+                    type_id: 511,
+                    message_id: 1,
+                    _received_time: None,
+                },
+                Ok(vec![ObsinnChunk {
+                    observations: vec![
                         ObsinnObs {
                             id: ObsinnId {
                                 param_code: "TJ".to_string(),
@@ -877,12 +914,26 @@ mod tests {
                             },
                             value: Scalar(None),
                         },
+                        ObsinnObs {
+                            id: ObsinnId {
+                                param_code: "X1R".to_string(),
+                                sensor_and_level: None,
+                            },
+                            value: NonScalar("24R"),
+                        },
+                        ObsinnObs {
+                            id: ObsinnId {
+                                param_code: "X2R".to_string(),
+                                sensor_and_level: None,
+                            },
+                            value: NonScalar("24L"),
+                        },
                     ],
                     timestamp: Utc.with_ymd_and_hms(2024, 9, 10, 0, 0, 0).unwrap(),
                     station_id: 18700,
                     type_id: 511,
                 }]),
-                "parameter with missing observations",
+                "special cases",
             ),
         ];
 
