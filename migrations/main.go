15a0a7a74942327220e28e475f6f2cca9ee94670
Manuel Carrer <manuel.carrer@zohomail.eu>
2024-12-28 15:17:39 +0100

    Remove comment from main

--- migrations/main.go
+++ migrations/main.go
@@ -18,16 +18,6 @@ type CmdArgs struct {
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-
-	// TODO: this should be printed in the help message
-	// The following env variables are required:
-	// 1. Dump
-	//   - kdvh: "KDVH_PROXY_CONN_STRING"
-	//   - kvalobs: "KVALOBS_CONN_STRING", "HISTKVALOBS_CONN_STRING"
-	//
-	// 2. Import
-	//   - kdvh: "LARD_CONN_STRING", "STINFO_CONN_STRING", "KDVH_PROXY_CONN_STRING"
-	//   - kvalobs: "LARD_CONN_STRING", "STINFO_CONN_STRING", "KVALOBS_CONN_STRING"
 	args := CmdArgs{}
 	parser := arg.MustParse(&args)
 
@@ -37,8 +27,7 @@ func main() {
 	case args.Kvalobs != nil:
 		args.Kvalobs.Execute(parser)
 	default:
-		fmt.Println("Error: passing a subcommand is required.")
-		fmt.Println()
+		fmt.Print("Error: passing a subcommand is required.\n\n")
 		parser.WriteHelp(os.Stdout)
 	}
 }
