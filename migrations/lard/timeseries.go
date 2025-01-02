35253208be190be5da48d55b1a4ba4521eb0351d
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-01-02 13:59:50 +0100

    Directly insert new timeseries instead of checking if it already exists in LARD

--- migrations/lard/timeseries.go
+++ migrations/lard/timeseries.go
@@ -16,63 +16,23 @@ type Label struct {
 	Level     *int32
 }
 
-func (l *Label) sensorLevelAreBothZero() bool {
-	if l.Sensor == nil || l.Level == nil {
-		return false
-	}
-	return *l.Level == 0 && *l.Sensor == 0
-}
-
+// Insert timeseries with given label and timespan, returning the timeseries ID
 func GetTimeseriesID(label *Label, timespan utils.TimeSpan, pool *pgxpool.Pool) (tsid int64, err error) {
-	// Query LARD labels table
-	err = pool.QueryRow(
-		context.TODO(),
-		`SELECT timeseries FROM labels.met
-            WHERE station_id = $1
-            AND param_id = $2
-            AND type_id = $3
-            AND (($4::int IS NULL AND lvl IS NULL) OR (lvl = $4))
-            AND (($5::int IS NULL AND sensor IS NULL) OR (sensor = $5))`,
-		label.StationID, label.ParamID, label.TypeID, label.Level, label.Sensor).Scan(&tsid)
-
-	// If timeseries exists, return its ID
-	if err == nil {
-		return tsid, nil
-	}
-
-	// In KDVH and Kvalobs sensor and level have default values, while in LARD they are NULL
-	// if Obsinn does not specify them. Therefore we need to check if sensor and level are NULL
-	// when they are both zero.
-	// FIXME(?): in some cases, level and sensor are marked with (0,0) in Obsinn,
-	// so there might be problems if a timeseries is not present in LARD at the time of importing
-	if label.sensorLevelAreBothZero() {
-		err := pool.QueryRow(
-			context.TODO(),
-			`SELECT timeseries FROM labels.met
-                WHERE station_id = $1
-                AND param_id = $2
-                AND type_id = $3
-                AND lvl IS NULL
-                AND sensor IS NULL`,
-			label.StationID, label.ParamID, label.TypeID).Scan(&tsid)
-
-		if err == nil {
-			return tsid, nil
-		}
-	}
-
-	// If none of the above worked insert a new timeseries
 	transaction, err := pool.Begin(context.TODO())
 	if err != nil {
 		return tsid, err
 	}
 	defer transaction.Rollback(context.TODO())
 
-	// TODO: should we set `deactivated` to true if `totime` is not NULL?
+	var deactivated bool
+	if timespan.To == nil {
+		deactivated = true
+	}
+
 	err = transaction.QueryRow(
 		context.TODO(),
-		`INSERT INTO public.timeseries (fromtime, totime) VALUES ($1, $2) RETURNING id`,
-		timespan.From, timespan.To,
+		`INSERT INTO public.timeseries (fromtime, totime, deactivated) VALUES ($1, $2, $3) RETURNING id`,
+		timespan.From, timespan.To, deactivated,
 	).Scan(&tsid)
 	if err != nil {
 		return tsid, err
