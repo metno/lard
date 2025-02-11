91594cf6da13fa497858af0fd182dcb49d3c09e7
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-02-11 13:30:07 +0100

    Add test for unique constraint on labels.obsinn

--- integration_tests/tests/end_to_end.rs
+++ integration_tests/tests/end_to_end.rs
@@ -449,11 +449,11 @@ async fn test_stations_endpoint_errors() {
 #[tokio::test]
 async fn test_latest_endpoint() {
     let cases = vec![
-        // without query
+        // without query param
         ("", 2),
-        // latest max age 1
+        // with latest max age, after old data
         ("?latest_max_age=2021-01-01T00:00:00Z", 2),
-        // latest max age 2
+        // with latest max age, before old data
         ("?latest_max_age=2019-01-01T00:00:00Z", 4),
     ];
     for (query, n_timeseries_found) in cases {
@@ -550,6 +550,68 @@ async fn test_timeslice_endpoint() {
             for (data, ts) in slice.data.iter().zip(&test_data) {
                 assert_eq!(data.station_id, ts.station_id);
             }
+        }
+    })
+    .await
+}
+
+#[tokio::test]
+async fn test_labels_obsinn_unique_constraint() {
+    e2e_test_wrapper(async {
+        let timestamp = Utc.with_ymd_and_hms(2024, 1, 31, 0, 0, 0).unwrap();
+        let params = vec![Param::new("TA")];
+        let station_id = 20001;
+
+        let test_data = [
+            TestData {
+                station_id,
+                params: params.clone(),
+                start_time: timestamp - Duration::hours(3),
+                period: Duration::hours(1),
+                type_id: 501,
+                len: 2,
+            },
+            TestData {
+                station_id,
+                params: params.clone(),
+                start_time: timestamp - Duration::days(3),
+                period: Duration::hours(1),
+                type_id: 501,
+                len: 24,
+            },
+        ];
+        let total_data_points: usize = test_data.iter().map(|ts| ts.len).sum();
+
+        let client = reqwest::Client::new();
+        for ts in &test_data {
+            let ingestor_resp = ingest_data(&client, ts.obsinn_message()).await;
+            assert_eq!(
+                ingestor_resp.res, 0,
+                "ingestor_resp.message: {}",
+                ingestor_resp.message
+            );
+        }
+
+        for param in params {
+            // TODO: start_time is needed because the endpoint handler would use the
+            // timeseries.fromtime column otherwise, which can return the "wrong" timestamp if
+            // newer data points are ingested first (like in our `test_data`).
+            // We should probably rethink our timeseries creation strategy
+            let url = format!(
+                "http://localhost:3000/stations/{}/params/{}?start_time=1950-01-01T00:00:00Z",
+                station_id, param.id
+            );
+
+            let resp = reqwest::get(url).await.unwrap();
+            let json: TimeseriesResp = resp.json().await.unwrap();
+
+            assert_eq!(json.tseries.len(), 1);
+
+            let Timeseries::Irregular(series) = &json.tseries[0] else {
+                panic!("Expected irrregular timeseries")
+            };
+
+            assert_eq!(series.data.len(), total_data_points);
         }
     })
     .await
