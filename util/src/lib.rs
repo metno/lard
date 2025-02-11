1cfaa405a9752bb7065b73149e12187b7f5629ac
Jo Asplin <jo.asplin@met.no>
2025-02-11 11:02:26 +0100

    Assumed Unix + added comments

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,27 +1,25 @@
 use tokio::signal;
 
-/// Returns a Future that completes once a signal to shutdown the service is caught.
+/// Returns a Future that completes once a relevant signal to shutdown the service is caught.
 pub async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c() // aka. SIGINT on Unix
-            .await
-            .expect("failed to install Ctrl+C (SIGINT) handler");
-    };
-
-    #[cfg(unix)]
+    // SIGTERM is the most important signal to handle since it is the one that is sent by
+    // systemd (as a result of commands like 'systemctl stop' or 'systemctl restart').
     let sigterm = async {
         signal::unix::signal(signal::unix::SignalKind::terminate())
             .expect("failed to install signal handler for SIGTERM")
             .recv()
             .await;
     };
-    #[cfg(not(unix))]
-    let sigterm = std::future::pending::<()>();
 
-    // TODO: add more signals that should result in a shutdown?
+    // SIGINT could also be relevant in some cases
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install signal handler for SIGINT (Ctrl-C)");
+    };
 
     tokio::select! {
-        _ = ctrl_c => {},
         _ = sigterm => {},
+        _ = ctrl_c => {},
     }
 }
