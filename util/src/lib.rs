f10ac0f297359cf3985a96e835cc84cdebd56124
Jo Asplin <jo.asplin@met.no>
2025-02-11 08:53:13 +0100

    Removed superfluous signals

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -9,46 +9,6 @@ pub async fn shutdown_signal() {
     };
 
     #[cfg(unix)]
-    let sighup = async {
-        signal::unix::signal(signal::unix::SignalKind::hangup())
-            .expect("failed to install signal handler for SIGHUP")
-            .recv()
-            .await;
-    };
-    #[cfg(not(unix))]
-    let sighup = std::future::pending::<()>();
-
-    #[cfg(unix)]
-    let sigquit = async {
-        signal::unix::signal(signal::unix::SignalKind::quit())
-            .expect("failed to install signal handler for SIGQUIT")
-            .recv()
-            .await;
-    };
-    #[cfg(not(unix))]
-    let sigquit = std::future::pending::<()>();
-
-    #[cfg(unix)]
-    let sigpipe = async {
-        signal::unix::signal(signal::unix::SignalKind::hangup())
-            .expect("failed to install signal handler for SIGPIPE")
-            .recv()
-            .await;
-    };
-    #[cfg(not(unix))]
-    let sigpipe = std::future::pending::<()>();
-
-    #[cfg(unix)]
-    let sigalrm = async {
-        signal::unix::signal(signal::unix::SignalKind::alarm())
-            .expect("failed to install signal handler for SIGALRM")
-            .recv()
-            .await;
-    };
-    #[cfg(not(unix))]
-    let sigalrm = std::future::pending::<()>();
-
-    #[cfg(unix)]
     let sigterm = async {
         signal::unix::signal(signal::unix::SignalKind::terminate())
             .expect("failed to install signal handler for SIGTERM")
@@ -58,25 +18,10 @@ pub async fn shutdown_signal() {
     #[cfg(not(unix))]
     let sigterm = std::future::pending::<()>();
 
-    #[cfg(unix)]
-    let sigchld = async {
-        signal::unix::signal(signal::unix::SignalKind::child())
-            .expect("failed to install signal handler for SIGCHLD")
-            .recv()
-            .await;
-    };
-    #[cfg(not(unix))]
-    let sigchld = std::future::pending::<()>();
-
     // TODO: add more signals that should result in a shutdown?
 
     tokio::select! {
         _ = ctrl_c => {},
-        _ = sighup => {},
-        _ = sigquit => {},
-        _ = sigpipe => {},
-        _ = sigalrm => {},
         _ = sigterm => {},
-        _ = sigchld => {},
     }
 }
