463436677e56b50ca8b15c15a634cad30e122abc
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-03-14 11:17:06 +0100

    Hack: remove restricted pool for now

--- migrations/lard/indices.go
+++ migrations/lard/indices.go
@@ -62,32 +62,33 @@ func CreateIndices(pool *Pool) {
 	ctx := context.Background()
 
 	group := errgroup.Group{}
-	pools := pool.GetSlice()
+	// pools := pool.GetSlice()
 	schemas := []Schema{{"public", "data"}, {"public", "nonscalar_data"}, {"legacy", "data"}}
+	p := pool.Open
 
-	for _, p := range pools {
-		if _, err := p.Exec(ctx, "SET maintenance_work_mem TO '2 GB'"); err != nil {
-			fmt.Println(err)
-		}
-		if _, err := p.Exec(ctx, "SET max_parallel_maintenance_workers TO 8"); err != nil {
-			fmt.Println(err)
-		}
-
-		for _, s := range schemas {
-			group.Go(func() error {
-				query := fmt.Sprintf("CREATE INDEX IF NOT EXISTS %s_timestamp_index ON %s.%s (obstime)", s.table, s.name, s.table)
-				return measureExec(ctx, query, p)
-			})
-			group.Go(func() error {
-				query := fmt.Sprintf("CREATE INDEX IF NOT EXISTS %s_timeseries_index ON %s.%s USING HASH (timeseries)", s.table, s.name, s.table)
-				return measureExec(ctx, query, p)
-			})
-			group.Go(func() error {
-				query := fmt.Sprintf("ALTER TABLE %s.%s ADD CONSTRAINT unique_%s_timeseries_obstime UNIQUE (timeseries, obstime)", s.name, s.table, s.table)
-				return measureExec(ctx, query, p)
-			})
-		}
+	// for _, p := range pools {
+	if _, err := p.Exec(ctx, "SET maintenance_work_mem TO '2 GB'"); err != nil {
+		fmt.Println(err)
 	}
+	if _, err := p.Exec(ctx, "SET max_parallel_maintenance_workers TO 8"); err != nil {
+		fmt.Println(err)
+	}
+
+	for _, s := range schemas {
+		group.Go(func() error {
+			query := fmt.Sprintf("CREATE INDEX IF NOT EXISTS %s_timestamp_index ON %s.%s (obstime)", s.table, s.name, s.table)
+			return measureExec(ctx, query, p)
+		})
+		group.Go(func() error {
+			query := fmt.Sprintf("CREATE INDEX IF NOT EXISTS %s_timeseries_index ON %s.%s USING HASH (timeseries)", s.table, s.name, s.table)
+			return measureExec(ctx, query, p)
+		})
+		group.Go(func() error {
+			query := fmt.Sprintf("ALTER TABLE %s.%s ADD CONSTRAINT unique_%s_timeseries_obstime UNIQUE (timeseries, obstime)", s.name, s.table, s.table)
+			return measureExec(ctx, query, p)
+		})
+	}
+	// }
 
 	if err := group.Wait(); err == nil {
 		fmt.Println(time.Now().Format(time.RFC3339), "Finished creating indices!")
@@ -97,12 +98,12 @@ func CreateIndices(pool *Pool) {
 	// and this setting is only used for index creation and vacuuming
 	// It might be worth also chaging work_mem (albeit it's a bit more dangerous since we need to figure out
 	// what our average/max query load looks like)
-	for _, p := range pools {
-		if _, err := p.Exec(ctx, "RESET maintenance_work_mem"); err != nil {
-			fmt.Println(err)
-		}
-		if _, err := p.Exec(ctx, "RESET max_parallel_maintenance_workers"); err != nil {
-			fmt.Println(err)
-		}
+	// for _, p := range pools {
+	if _, err := p.Exec(ctx, "RESET maintenance_work_mem"); err != nil {
+		fmt.Println(err)
 	}
+	if _, err := p.Exec(ctx, "RESET max_parallel_maintenance_workers"); err != nil {
+		fmt.Println(err)
+	}
+	// }
 }
--- migrations/lard/pool.go
+++ migrations/lard/pool.go
@@ -24,13 +24,14 @@ func NewLardPool(ctx context.Context) *Pool {
 		os.Exit(1)
 	}
 
-	restrictedPool, err := pgxpool.New(ctx, os.Getenv(LARD_OPEN_ENV_VAR))
-	if err != nil {
-		fmt.Println("Could not connect to Lard")
-		os.Exit(1)
-	}
+	// restrictedPool, err := pgxpool.New(ctx, os.Getenv(LARD_OPEN_ENV_VAR))
+	// if err != nil {
+	// 	fmt.Println("Could not connect to Lard")
+	// 	os.Exit(1)
+	// }
 
-	return &Pool{Open: openPool, Restricted: restrictedPool}
+	// return &Pool{Open: openPool, Restricted: restrictedPool}
+	return &Pool{Open: openPool}
 }
 
 func (pool *Pool) Close() {
