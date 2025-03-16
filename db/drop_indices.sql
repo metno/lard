82b4357bc4ed1dd53c13c0334e72b91a6b09394f
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-03-16 17:40:58 +0100

    Don't drop flags indices

--- db/drop_indices.sql
+++ db/drop_indices.sql
@@ -3,7 +3,7 @@ DECLARE
  i RECORD;
 BEGIN
     FOR i IN (SELECT schemaname, indexname fROM pg_indexes
-                WHERE schemaname IN ('public', 'flags', 'legacy')
+                WHERE schemaname IN ('public', 'legacy')
                 AND NOT indexdef LIKE '%UNIQUE%')
     LOOP
         EXECUTE format('DROP INDEX IF EXISTS %s.%s', i.schemaname, i.indexname);
