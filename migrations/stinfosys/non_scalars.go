fb2017f600c5f74105c700edcb4675f86953cb82
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-03-17 17:01:34 +0100

    Those should not use the logger

--- migrations/stinfosys/non_scalars.go
+++ migrations/stinfosys/non_scalars.go
@@ -2,21 +2,21 @@ package stinfosys
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/rs/zerolog/log"
 )
 
 func GetNonScalars(conn *pgx.Conn) []int32 {
 	rows, err := conn.Query(context.TODO(), "SELECT paramid FROM param WHERE scalar = false ORDER BY paramid")
 	if err != nil {
-		log.Error().Err(err).Msg("")
+		fmt.Println(err)
 		os.Exit(1)
 	}
 	nonscalars, err := pgx.CollectRows(rows, pgx.RowTo[int32])
 	if err != nil {
-		log.Error().Err(err).Msg("")
+		fmt.Println(err)
 		os.Exit(1)
 	}
 	return nonscalars
