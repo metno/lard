a79cbab6007827c86836543027ffefbc28adf9fd
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-03-20 14:55:26 +0100

    Remove KDVH fromtime/totime caching since those values have dubious nature

--- migrations/kdvh/import/cache.go
+++ migrations/kdvh/import/cache.go
@@ -1,32 +1,23 @@
 package port
 
 import (
-	"context"
 	"fmt"
 	"os"
-	"slices"
-	"time"
 
 	"github.com/gocarina/gocsv"
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rickb777/period"
 
 	kdvh "migrate/kdvh/db"
 	"migrate/lard"
 	"migrate/stinfosys"
-	"migrate/utils"
 )
 
 type Cache struct {
-	Offsets   OffsetMap
-	Timespans KDVHMap
-	Elements  stinfosys.ElemMap
-	Permits   stinfosys.PermitMaps
+	Offsets  OffsetMap
+	Elements stinfosys.ElemMap
+	Permits  stinfosys.PermitMaps
 }
-
-// Map of `from_time` and `to_time` for each (table, station, element) triplet. Not present for all parameters
-type KDVHMap = map[KDVHKey]utils.TimeSpan
 
 // Used for lookup of fromtime and totime from KDVH
 type KDVHKey struct {
@@ -44,10 +35,9 @@ func CacheMetadata(tables, stations, elements []string, database []*Table) *Cach
 	defer stconn.Close(ctx)
 
 	return &Cache{
-		Elements:  stinfosys.CacheElemMap(stconn),
-		Permits:   stinfosys.NewPermitTables(stconn),
-		Offsets:   cacheParamOffsets(),
-		Timespans: cacheKDVH(tables, stations, elements, database),
+		Elements: stinfosys.CacheElemMap(stconn),
+		Permits:  stinfosys.NewPermitTables(stconn),
+		Offsets:  cacheParamOffsets(),
 	}
 }
 
@@ -93,10 +83,6 @@ func GetTsInfoAndDbPool(table, element string, station int32, cache *Cache, pool
 	// No need to check for `!ok`, will default to 0 offset
 	offset := cache.Offsets[key.Inner]
 
-	// Get timespan found in KDVH
-	// No need to check for `!ok`, timespan will be ignored if not in the map
-	timespan, ok := cache.Timespans[key]
-
 	label := lard.Label{
 		StationID: station,
 		TypeID:    param.TypeID,
@@ -105,8 +91,7 @@ func GetTsInfoAndDbPool(table, element string, station int32, cache *Cache, pool
 		Level:     param.Hlevel,
 	}
 
-	tsSpan := utils.TimeSpan{From: &param.Fromtime, To: timespan.To}
-	tsid, err := label.CreateKDVHTimeseries(element, table, tsSpan, permit, innerPool)
+	tsid, err := label.CreateKDVHTimeseries(element, table, &param.Fromtime, permit, innerPool)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -117,78 +102,11 @@ func GetTsInfoAndDbPool(table, element string, station int32, cache *Cache, pool
 		Element:  element,
 		Offset:   offset,
 		IsScalar: param.IsScalar,
-		Timespan: timespan,
 	}, innerPool, nil
 }
 
 func newKDVHKey(elem, table string, stnr int32) KDVHKey {
 	return KDVHKey{stinfosys.Key{ElemCode: elem, TableName: table}, stnr}
-}
-
-// Cache timeseries timespan from KDVH
-// TODO: we should dump these tables! We will not be able to connect to KDVH when it's taken down
-func cacheKDVH(tables, stations, elements []string, database []*Table) KDVHMap {
-	cache := make(KDVHMap)
-
-	fmt.Printf("%-50s", "Caching KDVH metadata via proxy... ")
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
-
-	conn, err := pgx.Connect(ctx, os.Getenv(kdvh.KDVH_ENV_VAR))
-	if err != nil {
-		fmt.Println("\n", "Could not connect to KDVH proxy. Make sure to be connected to the VPN:", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.TODO())
-
-	for _, t := range database {
-		if len(tables) > 0 && !slices.Contains(tables, t.TableName) {
-			continue
-		}
-
-		// TODO: probably need to sanitize these inputs
-		query := fmt.Sprintf(
-			`SELECT table_name, stnr, elem_code, fdato, tdato FROM %s
-                WHERE ($1::bigint[] = '{}' OR stnr = ANY($1))
-                AND ($2::text[] = '{}' OR elem_code = ANY($2))`,
-			t.ElemTableName,
-		)
-
-		rows, err := conn.Query(context.TODO(), query, stations, elements)
-		if err != nil {
-			fmt.Println("\n", err)
-			os.Exit(1)
-		}
-
-		for rows.Next() {
-			var key KDVHKey
-			var span utils.TimeSpan
-
-			err := rows.Scan(
-				&key.Inner.TableName,
-				&key.Station,
-				&key.Inner.ElemCode,
-				&span.From,
-				&span.To,
-			)
-
-			if err != nil {
-				fmt.Println("\n", err)
-				os.Exit(1)
-			}
-
-			cache[key] = span
-		}
-
-		if rows.Err() != nil {
-			fmt.Println("\n", rows.Err())
-			os.Exit(1)
-		}
-
-	}
-
-	fmt.Println("Done!")
-	return cache
 }
 
 // Caches how to modify the obstime (in KDVH) for certain paramids
