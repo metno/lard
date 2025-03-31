metno/PODA

Kvalobs: move dump and import functions back into their respective packages

migrations/kvalobs/import/csv_parsers.go
@@ -1,15 +1,29 @@
-package db
+package port
 
 import (
 	"bufio"
+	kvalobs "migrate/kvalobs/db"
 	"migrate/lard"
+	"os"
 	"slices"
 	"strconv"
 	"strings"
 	"time"
 )
 
-func parseDataCSV(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, [][]any, error) {
+// TODO: Move back to import
+// and define a return struct?
+
+func parseDataCSV(tsid int32, file *os.File) ([][]any, [][]any, error) {
+	scanner := bufio.NewScanner(file)
+
+	// Parse number of rows
+	scanner.Scan()
+	rowCount, _ := strconv.Atoi(scanner.Text())
+
+	// Skip header
+	scanner.Scan()
+
 	data := make([][]any, 0, rowCount)
 	flags := make([][]any, 0, rowCount)
 	var originalPtr, correctedPtr *float32
@@ -37,10 +51,10 @@ func parseDataCSV(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, []
 		corrected := float32(corrected64)
 
 		// Filter out special values that in Kvalobs stand for null observations
-		if !slices.Contains(NULL_VALUES, original) {
+		if !slices.Contains(kvalobs.NULL_VALUES, original) {
 			originalPtr = &original
 		}
-		if !slices.Contains(NULL_VALUES, corrected) {
+		if !slices.Contains(kvalobs.NULL_VALUES, corrected) {
 			correctedPtr = &corrected
 		}
 
@@ -74,7 +88,16 @@ func parseDataCSV(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, []
 }
 
 // Text obs are not flagged
-func parseTextCSV(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
+func parseTextCSV(tsid int32, file *os.File) ([][]any, error) {
+	scanner := bufio.NewScanner(file)
+
+	// Parse number of rows
+	scanner.Scan()
+	rowCount, _ := strconv.Atoi(scanner.Text())
+
+	// Skip header
+	scanner.Scan()
+
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime
@@ -101,7 +124,16 @@ func parseTextCSV(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, er
 // but should instead be treated as scalars
 // TODO: I'm not sure these params should be scalars given that the other cloud types are not.
 // Should all cloud types be integers or text?
-func parseMetarCloudType(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
+func parseMetarCloudType(tsid int32, file *os.File) ([][]any, error) {
+	scanner := bufio.NewScanner(file)
+
+	// Parse number of rows
+	scanner.Scan()
+	rowCount, _ := strconv.Atoi(scanner.Text())
+
+	// Skip header
+	scanner.Scan()
+
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime
@@ -134,7 +166,16 @@ func parseMetarCloudType(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]
 
 // Function for paramids 305, 306, 307, 308 that were stored as scalar data
 // but should be treated as text
-func parseSpecialCloudType(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
+func parseSpecialCloudType(tsid int32, file *os.File) ([][]any, error) {
+	scanner := bufio.NewScanner(file)
+
+	// Parse number of rows
+	scanner.Scan()
+	rowCount, _ := strconv.Atoi(scanner.Text())
+
+	// Skip header
+	scanner.Scan()
+
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime, corrected, controlinfo, useinfo, cfailed

migrations/kvalobs/import/import.go
@@ -12,27 +12,27 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	kvalobs "migrate/kvalobs/db"
-	"migrate/kvalobs/import/cache"
 	"migrate/lard"
 	"migrate/utils"
 )
 
 // NOTE: we return the number of inserted rows for the tests
-func ImportTable(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, config *Config) (int64, error) {
-	fmt.Printf("Importing from %q...\n", table.Path)
+func (table *Table) Import(path string, cache *Cache, pool *pgxpool.Pool, config *Config) (int64, error) {
+	fmt.Printf("Importing from %q...\n", path)
 	defer fmt.Println(strings.Repeat("- ", 40))
 
-	stations, err := os.ReadDir(table.Path)
+	stations, err := os.ReadDir(path)
 	if err != nil {
 		slog.Error(err.Error())
 		return 0, err
 	}
 
 	// Used to limit number of spawned threads
-	// Too many threads can lead to an OOM kill, due to slice allocations in parseData
+	// Too many threads can lead to an OOM kill, due to slice allocations in table.Import
 	semaphore := make(chan struct{}, config.MaxWorkers)
+	bar := utils.NewBar(len(stations), fmt.Sprintf("Importing %s stations...", table.Name))
+	bar.RenderBlank()
 
-	fmt.Printf("Number of stations to import: %d...\n", len(stations))
 	var rowsInserted int64
 	for _, station := range stations {
 		stnr, err := strconv.ParseInt(station.Name(), 10, 32)
@@ -40,15 +40,12 @@ func ImportTable(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, c
 			continue
 		}
 
-		stationDir := filepath.Join(table.Path, station.Name())
+		stationDir := filepath.Join(path, station.Name())
 		labels, err := os.ReadDir(stationDir)
 		if err != nil {
 			slog.Warn(err.Error())
 			continue
 		}
-
-		bar := utils.NewBar(len(labels), fmt.Sprintf("%10s", station.Name()))
-		bar.RenderBlank()
 
 		var wg sync.WaitGroup
 		for _, file := range labels {
@@ -57,7 +54,6 @@ func ImportTable(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, c
 
 			go func() {
 				defer func() {
-					bar.Add(1)
 					<-semaphore
 					wg.Done()
 				}()
@@ -73,86 +69,97 @@ func ImportTable(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, c
 				}
 
 				logStr := label.LogStr()
-				// Check if data for this station/element is restricted
-				if !cache.TimeseriesIsOpen(label.StationID, label.TypeID, label.ParamID) {
-					// TODO: eventually use this to choose which table to use on insert
-					slog.Warn(logStr + "timeseries data is restricted, skipping")
-					return
-				}
-
-				tsTimespan, err := cache.GetSeriesTimespan(label)
+				tsid, err := getTsid(label, cache, pool)
 				if err != nil {
 					slog.Error(logStr + err.Error())
-					return
-				}
-
-				// TODO: figure out where to get fromtime, kvalobs directly? Stinfosys?
-				tsid, err := lard.GetTimeseriesID(label.ToLard(), tsTimespan, pool)
-				if err != nil {
-					slog.Error(logStr + err.Error())
-					return
 				}
 
 				filename := filepath.Join(stationDir, file.Name())
-				count, err := table.Import(tsid, label, filename, logStr, pool)
+				file, err := os.Open(filename)
 				if err != nil {
-					// Logged inside table.Import
+					slog.Error(logStr + err.Error())
 					return
 				}
+				defer file.Close()
 
-				rowsInserted += count
+				count, err := table.ImportFn(file, tsid, label, logStr, pool)
+				if err == nil {
+					rowsInserted += count
+				}
 			}()
 		}
 		wg.Wait()
+		bar.Add(1)
 	}
 
-	outputStr := fmt.Sprintf("%v: %v total rows inserted", table.Path, rowsInserted)
+	outputStr := fmt.Sprintf("%v: %v total rows inserted", path, rowsInserted)
 	slog.Info(outputStr)
 	fmt.Println(outputStr)
 
 	return rowsInserted, nil
 }
 
-func ImportAllTimespans(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, config *Config) (int64, error) {
-	timespans, err := os.ReadDir(table.Path)
+func getTsid(label *kvalobs.Label, cache *Cache, pool *pgxpool.Pool) (int32, error) {
+	// Check if data for this station/element is restricted
+	if !cache.TimeseriesIsOpen(label.StationID, label.TypeID, label.ParamID) {
+		// TODO: eventually use this to choose which table to use on insert
+		return 0, fmt.Errorf("timeseries data is restricted, skipping")
+	}
+
+	tsTimespan, err := cache.GetSeriesTimespan(label)
+	if err != nil {
+		return 0, err
+	}
+
+	// TODO: figure out where to get fromtime, kvalobs directly? Stinfosys?
+	tsid, err := lard.GetTimeseriesID(label.ToLard(), tsTimespan, pool)
+	if err != nil {
+		return 0, err
+	}
+
+	return tsid, nil
+}
+
+func (table *Table) ImportAllTimespans(path string, cache *Cache, pool *pgxpool.Pool, config *Config) (int64, error) {
+	timespans, err := os.ReadDir(path)
 	if err != nil {
 		slog.Error(err.Error())
 		return 0, err
 	}
 
-	path := table.Path
 	for _, span := range timespans {
 		if !span.IsDir() {
 			continue
 		}
 
-		table.SetPath(filepath.Join(path, span.Name()))
-		ImportTable(table, cache, pool, config)
+		table.Import(filepath.Join(path, span.Name()), cache, pool, config)
 	}
 
 	return 0, nil
 }
 
-func ImportDB(database *kvalobs.DB, cache *cache.Cache, pool *pgxpool.Pool, config *Config) {
-	for name, table := range database.Tables {
+func (db *Database) Import(cache *Cache, pool *pgxpool.Pool, config *Config) {
+	for name, table := range db.Tables {
 		if !utils.StringIsEmptyOrEqual(config.Table, name) {
 			continue
 		}
 
-		// dumps/<db_name>/<table_name>/(<SpanDir>/)
-		table.SetPath(filepath.Join(
+		// <base_path>/<db_name>/<table_name>/<timespan>/
+		path := filepath.Join(
 			config.Path,
-			database.Name,
+			db.Name,
 			table.Name,
 			config.SpanDir,
-		))
+		)
+
 		// dumps/<db_name>/<table_name>/<timespan>/import_<now>.log
-		utils.SetLogFile(config.Path, "import")
+		handle := utils.SetLogFile(config.Path, "import")
+		defer handle.Close()
 
 		if config.SpanDir == "" {
-			ImportAllTimespans(table, cache, pool, config)
+			table.ImportAllTimespans(path, cache, pool, config)
 		} else {
-			ImportTable(table, cache, pool, config)
+			table.Import(path, cache, pool, config)
 		}
 	}
 }

migrations/kvalobs/import/import_functions.go
@@ -1,0 +1,88 @@
+package port
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"migrate/kvalobs/db"
+	"migrate/lard"
+)
+
+// NOTE:
+// - for both kvalobs and histkvalobs:
+//      - all stinfo non-scalar params that can be found in Kvalobs are stored in `text_data`
+//      - 305, 306, 307, 308 are also in `data` but should be treated as `text_data` -> Special Cloud Types
+//          => should always use readDataCSV and lard.InsertData for these
+// - only for histkvalobs
+//      - 2751, 2752, 2753, 2754 are in `text_data` but should be treated as `data`? -> Metar Cloud types
+
+func importData(file *os.File, tsid int32, label *db.Label, logStr string, pool *pgxpool.Pool) (int64, error) {
+	if label.IsSpecialCloudType() {
+		text, err := parseSpecialCloudType(tsid, file)
+		if err != nil {
+			slog.Error(logStr + err.Error())
+			return 0, err
+		}
+
+		count, err := lard.InsertTextData(text, pool, logStr)
+		if err != nil {
+			slog.Error(logStr + err.Error())
+			return 0, err
+		}
+
+		return count, nil
+	}
+
+	data, flags, err := parseDataCSV(tsid, file)
+	if err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	count, err := lard.InsertData(data, pool, logStr)
+	if err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	if err := lard.InsertFlags(flags, pool, logStr); err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func importText(file *os.File, tsid int32, label *db.Label, logStr string, pool *pgxpool.Pool) (int64, error) {
+	if label.IsMetarCloudType() {
+		data, err := parseMetarCloudType(tsid, file)
+		if err != nil {
+			slog.Error(logStr + err.Error())
+			return 0, err
+		}
+
+		count, err := lard.InsertData(data, pool, logStr)
+		if err != nil {
+			slog.Error(logStr + err.Error())
+			return 0, err
+		}
+
+		return count, nil
+	}
+
+	text, err := parseTextCSV(tsid, file)
+	if err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	count, err := lard.InsertTextData(text, pool, logStr)
+	if err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	return count, nil
+}

migrations/kvalobs/import/tables.go
@@ -1,0 +1,33 @@
+package port
+
+import (
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	kvalobs "migrate/kvalobs/db"
+)
+
+type ImportFunc func(file *os.File, tsid int32, label *kvalobs.Label, logStr string, pool *pgxpool.Pool) (int64, error)
+type Table struct {
+	Name     string
+	ImportFn ImportFunc // Function that parses dumps and ingests observations into LARD
+}
+
+type Database struct {
+	Name       string
+	Tables     map[string]*Table
+	ConnEnvVar string
+}
+
+func InitImportDBs() map[string]*Database {
+	tables := map[string]*Table{
+		kvalobs.DataTableName: {Name: kvalobs.DataTableName, ImportFn: importData},
+		kvalobs.TextTableName: {Name: kvalobs.TextTableName, ImportFn: importText},
+	}
+
+	return map[string]*Database{
+		kvalobs.KvDbName:   {Name: kvalobs.KvDbName, ConnEnvVar: kvalobs.KvEnvVar, Tables: tables},
+		kvalobs.HistDbName: {Name: kvalobs.HistDbName, ConnEnvVar: kvalobs.HistEnvVar, Tables: tables},
+	}
+}

metno/lard

migrations/kvalobs/import/csv_parsers.go
@@ -1,15 +1,29 @@
-package db
+package port
 
 import (
 	"bufio"
+	kvalobs "migrate/kvalobs/db"
 	"migrate/lard"
+	"os"
 	"slices"
 	"strconv"
 	"strings"
 	"time"
 )
 
-func parseDataCSV(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, [][]any, error) {
+// TODO: Move back to import
+// and define a return struct?
+
+func parseDataCSV(tsid int64, file *os.File) ([][]any, [][]any, error) {
+	scanner := bufio.NewScanner(file)
+
+	// Parse number of rows
+	scanner.Scan()
+	rowCount, _ := strconv.Atoi(scanner.Text())
+
+	// Skip header
+	scanner.Scan()
+
 	data := make([][]any, 0, rowCount)
 	flags := make([][]any, 0, rowCount)
 	var originalPtr, correctedPtr *float64
@@ -34,10 +48,10 @@ func parseDataCSV(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, []
 		}
 
 		// Filter out special values that in Kvalobs stand for null observations
-		if !slices.Contains(NULL_VALUES, original) {
+		if !slices.Contains(kvalobs.NULL_VALUES, original) {
 			originalPtr = &original
 		}
-		if !slices.Contains(NULL_VALUES, corrected) {
+		if !slices.Contains(kvalobs.NULL_VALUES, corrected) {
 			correctedPtr = &corrected
 		}
 
@@ -71,7 +85,16 @@ func parseDataCSV(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, []
 }
 
 // Text obs are not flagged
-func parseTextCSV(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
+func parseTextCSV(tsid int64, file *os.File) ([][]any, error) {
+	scanner := bufio.NewScanner(file)
+
+	// Parse number of rows
+	scanner.Scan()
+	rowCount, _ := strconv.Atoi(scanner.Text())
+
+	// Skip header
+	scanner.Scan()
+
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime
@@ -98,7 +121,16 @@ func parseTextCSV(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, er
 // but should instead be treated as scalars
 // TODO: I'm not sure these params should be scalars given that the other cloud types are not.
 // Should all cloud types be integers or text?
-func parseMetarCloudType(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
+func parseMetarCloudType(tsid int64, file *os.File) ([][]any, error) {
+	scanner := bufio.NewScanner(file)
+
+	// Parse number of rows
+	scanner.Scan()
+	rowCount, _ := strconv.Atoi(scanner.Text())
+
+	// Skip header
+	scanner.Scan()
+
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime
@@ -130,7 +162,16 @@ func parseMetarCloudType(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]
 
 // Function for paramids 305, 306, 307, 308 that were stored as scalar data
 // but should be treated as text
-func parseSpecialCloudType(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
+func parseSpecialCloudType(tsid int64, file *os.File) ([][]any, error) {
+	scanner := bufio.NewScanner(file)
+
+	// Parse number of rows
+	scanner.Scan()
+	rowCount, _ := strconv.Atoi(scanner.Text())
+
+	// Skip header
+	scanner.Scan()
+
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime, corrected, controlinfo, useinfo, cfailed

migrations/kvalobs/import/import.go
@@ -12,27 +12,27 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	kvalobs "migrate/kvalobs/db"
-	"migrate/kvalobs/import/cache"
 	"migrate/lard"
 	"migrate/utils"
 )
 
 // NOTE: we return the number of inserted rows for the tests
-func ImportTable(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, config *Config) (int64, error) {
-	fmt.Printf("Importing from %q...\n", table.Path)
+func (table *Table) Import(path string, cache *Cache, pool *pgxpool.Pool, config *Config) (int64, error) {
+	fmt.Printf("Importing from %q...\n", path)
 	defer fmt.Println(strings.Repeat("- ", 40))
 
-	stations, err := os.ReadDir(table.Path)
+	stations, err := os.ReadDir(path)
 	if err != nil {
 		slog.Error(err.Error())
 		return 0, err
 	}
 
 	// Used to limit number of spawned threads
-	// Too many threads can lead to an OOM kill, due to slice allocations in parseData
+	// Too many threads can lead to an OOM kill, due to slice allocations in table.Import
 	semaphore := make(chan struct{}, config.MaxWorkers)
+	bar := utils.NewBar(len(stations), fmt.Sprintf("Importing %s stations...", table.Name))
+	bar.RenderBlank()
 
-	fmt.Printf("Number of stations to import: %d...\n", len(stations))
 	var rowsInserted int64
 	for _, station := range stations {
 		stnr, err := strconv.ParseInt(station.Name(), 10, 32)
@@ -40,15 +40,12 @@ func ImportTable(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, c
 			continue
 		}
 
-		stationDir := filepath.Join(table.Path, station.Name())
+		stationDir := filepath.Join(path, station.Name())
 		labels, err := os.ReadDir(stationDir)
 		if err != nil {
 			slog.Warn(err.Error())
 			continue
 		}
-
-		bar := utils.NewBar(len(labels), fmt.Sprintf("%10s", station.Name()))
-		bar.RenderBlank()
 
 		var wg sync.WaitGroup
 		for _, file := range labels {
@@ -57,7 +54,6 @@ func ImportTable(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, c
 
 			go func() {
 				defer func() {
-					bar.Add(1)
 					<-semaphore
 					wg.Done()
 				}()
@@ -73,86 +69,97 @@ func ImportTable(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, c
 				}
 
 				logStr := label.LogStr()
-				// Check if data for this station/element is restricted
-				if !cache.TimeseriesIsOpen(label.StationID, label.TypeID, label.ParamID) {
-					// TODO: eventually use this to choose which table to use on insert
-					slog.Warn(logStr + "timeseries data is restricted, skipping")
-					return
-				}
-
-				tsTimespan, err := cache.GetSeriesTimespan(label)
+				tsid, err := getTsid(label, cache, pool)
 				if err != nil {
 					slog.Error(logStr + err.Error())
-					return
-				}
-
-				// TODO: figure out where to get fromtime, kvalobs directly? Stinfosys?
-				tsid, err := lard.GetTimeseriesID(label.ToLard(), tsTimespan, pool)
-				if err != nil {
-					slog.Error(logStr + err.Error())
-					return
 				}
 
 				filename := filepath.Join(stationDir, file.Name())
-				count, err := table.Import(tsid, label, filename, logStr, pool)
+				file, err := os.Open(filename)
 				if err != nil {
-					// Logged inside table.Import
+					slog.Error(logStr + err.Error())
 					return
 				}
+				defer file.Close()
 
-				rowsInserted += count
+				count, err := table.ImportFn(file, tsid, label, logStr, pool)
+				if err == nil {
+					rowsInserted += count
+				}
 			}()
 		}
 		wg.Wait()
+		bar.Add(1)
 	}
 
-	outputStr := fmt.Sprintf("%v: %v total rows inserted", table.Path, rowsInserted)
+	outputStr := fmt.Sprintf("%v: %v total rows inserted", path, rowsInserted)
 	slog.Info(outputStr)
 	fmt.Println(outputStr)
 
 	return rowsInserted, nil
 }
 
-func ImportAllTimespans(table *kvalobs.Table, cache *cache.Cache, pool *pgxpool.Pool, config *Config) (int64, error) {
-	timespans, err := os.ReadDir(table.Path)
+func getTsid(label *kvalobs.Label, cache *Cache, pool *pgxpool.Pool) (int64, error) {
+	// Check if data for this station/element is restricted
+	if !cache.TimeseriesIsOpen(label.StationID, label.TypeID, label.ParamID) {
+		// TODO: eventually use this to choose which table to use on insert
+		return 0, fmt.Errorf("timeseries data is restricted, skipping")
+	}
+
+	tsTimespan, err := cache.GetSeriesTimespan(label)
+	if err != nil {
+		return 0, err
+	}
+
+	// TODO: figure out where to get fromtime, kvalobs directly? Stinfosys?
+	tsid, err := lard.GetTimeseriesID(label.ToLard(), tsTimespan, pool)
+	if err != nil {
+		return 0, err
+	}
+
+	return tsid, nil
+}
+
+func (table *Table) ImportAllTimespans(path string, cache *Cache, pool *pgxpool.Pool, config *Config) (int64, error) {
+	timespans, err := os.ReadDir(path)
 	if err != nil {
 		slog.Error(err.Error())
 		return 0, err
 	}
 
-	path := table.Path
 	for _, span := range timespans {
 		if !span.IsDir() {
 			continue
 		}
 
-		table.SetPath(filepath.Join(path, span.Name()))
-		ImportTable(table, cache, pool, config)
+		table.Import(filepath.Join(path, span.Name()), cache, pool, config)
 	}
 
 	return 0, nil
 }
 
-func ImportDB(database *kvalobs.DB, cache *cache.Cache, pool *pgxpool.Pool, config *Config) {
-	for name, table := range database.Tables {
+func (db *Database) Import(cache *Cache, pool *pgxpool.Pool, config *Config) {
+	for name, table := range db.Tables {
 		if !utils.StringIsEmptyOrEqual(config.Table, name) {
 			continue
 		}
 
-		// dumps/<db_name>/<table_name>/(<SpanDir>/)
-		table.SetPath(filepath.Join(
+		// <base_path>/<db_name>/<table_name>/<timespan>/
+		path := filepath.Join(
 			config.Path,
-			database.Name,
+			db.Name,
 			table.Name,
 			config.SpanDir,
-		))
+		)
+
 		// dumps/<db_name>/<table_name>/<timespan>/import_<now>.log
-		utils.SetLogFile(config.Path, "import")
+		handle := utils.SetLogFile(config.Path, "import")
+		defer handle.Close()
 
 		if config.SpanDir == "" {
-			ImportAllTimespans(table, cache, pool, config)
+			table.ImportAllTimespans(path, cache, pool, config)
 		} else {
-			ImportTable(table, cache, pool, config)
+			table.Import(path, cache, pool, config)
 		}
 	}
 }

migrations/kvalobs/import/import_functions.go
@@ -1,0 +1,88 @@
+package port
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"migrate/kvalobs/db"
+	"migrate/lard"
+)
+
+// NOTE:
+// - for both kvalobs and histkvalobs:
+//      - all stinfo non-scalar params that can be found in Kvalobs are stored in `text_data`
+//      - 305, 306, 307, 308 are also in `data` but should be treated as `text_data` -> Special Cloud Types
+//          => should always use readDataCSV and lard.InsertData for these
+// - only for histkvalobs
+//      - 2751, 2752, 2753, 2754 are in `text_data` but should be treated as `data`? -> Metar Cloud types
+
+func importData(file *os.File, tsid int64, label *db.Label, logStr string, pool *pgxpool.Pool) (int64, error) {
+	if label.IsSpecialCloudType() {
+		text, err := parseSpecialCloudType(tsid, file)
+		if err != nil {
+			slog.Error(logStr + err.Error())
+			return 0, err
+		}
+
+		count, err := lard.InsertTextData(text, pool, logStr)
+		if err != nil {
+			slog.Error(logStr + err.Error())
+			return 0, err
+		}
+
+		return count, nil
+	}
+
+	data, flags, err := parseDataCSV(tsid, file)
+	if err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	count, err := lard.InsertData(data, pool, logStr)
+	if err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	if err := lard.InsertFlags(flags, pool, logStr); err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func importText(file *os.File, tsid int64, label *db.Label, logStr string, pool *pgxpool.Pool) (int64, error) {
+	if label.IsMetarCloudType() {
+		data, err := parseMetarCloudType(tsid, file)
+		if err != nil {
+			slog.Error(logStr + err.Error())
+			return 0, err
+		}
+
+		count, err := lard.InsertData(data, pool, logStr)
+		if err != nil {
+			slog.Error(logStr + err.Error())
+			return 0, err
+		}
+
+		return count, nil
+	}
+
+	text, err := parseTextCSV(tsid, file)
+	if err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	count, err := lard.InsertTextData(text, pool, logStr)
+	if err != nil {
+		slog.Error(logStr + err.Error())
+		return 0, err
+	}
+
+	return count, nil
+}

migrations/kvalobs/import/tables.go
@@ -1,0 +1,33 @@
+package port
+
+import (
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	kvalobs "migrate/kvalobs/db"
+)
+
+type ImportFunc func(file *os.File, tsid int64, label *kvalobs.Label, logStr string, pool *pgxpool.Pool) (int64, error)
+type Table struct {
+	Name     string
+	ImportFn ImportFunc // Function that parses dumps and ingests observations into LARD
+}
+
+type Database struct {
+	Name       string
+	Tables     map[string]*Table
+	ConnEnvVar string
+}
+
+func InitImportDBs() map[string]*Database {
+	tables := map[string]*Table{
+		kvalobs.DataTableName: {Name: kvalobs.DataTableName, ImportFn: importData},
+		kvalobs.TextTableName: {Name: kvalobs.TextTableName, ImportFn: importText},
+	}
+
+	return map[string]*Database{
+		kvalobs.KvDbName:   {Name: kvalobs.KvDbName, ConnEnvVar: kvalobs.KvEnvVar, Tables: tables},
+		kvalobs.HistDbName: {Name: kvalobs.HistDbName, ConnEnvVar: kvalobs.HistEnvVar, Tables: tables},
+	}
+}