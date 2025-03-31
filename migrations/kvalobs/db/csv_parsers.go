metno/PODA

Kvalobs: dump and import to/from timespan dirs

migrations/kvalobs/db/csv_parsers.go
@@ -3,14 +3,13 @@ package db
 import (
 	"bufio"
 	"migrate/lard"
-	"migrate/utils"
 	"slices"
 	"strconv"
 	"strings"
 	"time"
 )
 
-func parseDataCSV(tsid int32, rowCount int, timespan *utils.TimeSpan, scanner *bufio.Scanner) ([][]any, [][]any, error) {
+func parseDataCSV(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, [][]any, error) {
 	data := make([][]any, 0, rowCount)
 	flags := make([][]any, 0, rowCount)
 	var originalPtr, correctedPtr *float32
@@ -22,13 +21,6 @@ func parseDataCSV(tsid int32, rowCount int, timespan *utils.TimeSpan, scanner *b
 		obstime, err := time.Parse(time.RFC3339, fields[0])
 		if err != nil {
 			return nil, nil, err
-		}
-
-		if timespan.From != nil && obstime.Sub(*timespan.From) < 0 {
-			continue
-		}
-		if timespan.To != nil && obstime.Sub(*timespan.To) > 0 {
-			break
 		}
 
 		obsvalue64, err := strconv.ParseFloat(fields[1], 32)
@@ -82,7 +74,7 @@ func parseDataCSV(tsid int32, rowCount int, timespan *utils.TimeSpan, scanner *b
 }
 
 // Text obs are not flagged
-func parseTextCSV(tsid int32, rowCount int, timespan *utils.TimeSpan, scanner *bufio.Scanner) ([][]any, error) {
+func parseTextCSV(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime
@@ -91,13 +83,6 @@ func parseTextCSV(tsid int32, rowCount int, timespan *utils.TimeSpan, scanner *b
 		obstime, err := time.Parse(time.RFC3339, fields[0])
 		if err != nil {
 			return nil, err
-		}
-
-		if timespan.From != nil && obstime.Sub(*timespan.From) < 0 {
-			continue
-		}
-		if timespan.To != nil && obstime.Sub(*timespan.To) > 0 {
-			break
 		}
 
 		lardObs := lard.TextObs{
@@ -116,7 +101,7 @@ func parseTextCSV(tsid int32, rowCount int, timespan *utils.TimeSpan, scanner *b
 // but should instead be treated as scalars
 // TODO: I'm not sure these params should be scalars given that the other cloud types are not.
 // Should all cloud types be integers or text?
-func parseMetarCloudType(tsid int32, rowCount int, timespan *utils.TimeSpan, scanner *bufio.Scanner) ([][]any, error) {
+func parseMetarCloudType(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime
@@ -125,13 +110,6 @@ func parseMetarCloudType(tsid int32, rowCount int, timespan *utils.TimeSpan, sca
 		obstime, err := time.Parse(time.RFC3339, fields[0])
 		if err != nil {
 			return nil, err
-		}
-
-		if timespan.From != nil && obstime.Sub(*timespan.From) < 0 {
-			continue
-		}
-		if timespan.To != nil && obstime.Sub(*timespan.To) > 0 {
-			break
 		}
 
 		val, err := strconv.ParseFloat(fields[1], 32)
@@ -156,7 +134,7 @@ func parseMetarCloudType(tsid int32, rowCount int, timespan *utils.TimeSpan, sca
 
 // Function for paramids 305, 306, 307, 308 that were stored as scalar data
 // but should be treated as text
-func parseSpecialCloudType(tsid int32, rowCount int, timespan *utils.TimeSpan, scanner *bufio.Scanner) ([][]any, error) {
+func parseSpecialCloudType(tsid int32, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime, corrected, controlinfo, useinfo, cfailed
@@ -166,13 +144,6 @@ func parseSpecialCloudType(tsid int32, rowCount int, timespan *utils.TimeSpan, s
 		obstime, err := time.Parse(time.RFC3339, fields[0])
 		if err != nil {
 			return nil, err
-		}
-
-		if timespan.From != nil && obstime.Sub(*timespan.From) < 0 {
-			continue
-		}
-		if timespan.To != nil && obstime.Sub(*timespan.To) > 0 {
-			break
 		}
 
 		lardObs := lard.TextObs{

migrations/kvalobs/db/import_functions.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"log/slog"
 	"migrate/lard"
-	"migrate/utils"
 	"os"
 	"strconv"
 
@@ -19,7 +18,7 @@ import (
 // - only for histkvalobs
 //      - 2751, 2752, 2753, 2754 are in `text_data` but should be treated as `data`?
 
-func importData(tsid int32, label *Label, filename, logStr string, timespan *utils.TimeSpan, pool *pgxpool.Pool) (int64, error) {
+func importData(tsid int32, label *Label, filename, logStr string, pool *pgxpool.Pool) (int64, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		slog.Error(logStr + err.Error())
@@ -37,7 +36,7 @@ func importData(tsid int32, label *Label, filename, logStr string, timespan *uti
 	scanner.Scan()
 
 	if label.IsSpecialCloudType() {
-		text, err := parseSpecialCloudType(tsid, rowCount, timespan, scanner)
+		text, err := parseSpecialCloudType(tsid, rowCount, scanner)
 		if err != nil {
 			slog.Error(logStr + err.Error())
 			return 0, err
@@ -52,7 +51,7 @@ func importData(tsid int32, label *Label, filename, logStr string, timespan *uti
 		return count, nil
 	}
 
-	data, flags, err := parseDataCSV(tsid, rowCount, timespan, scanner)
+	data, flags, err := parseDataCSV(tsid, rowCount, scanner)
 	count, err := lard.InsertData(data, pool, logStr)
 	if err != nil {
 		slog.Error(logStr + err.Error())
@@ -67,7 +66,7 @@ func importData(tsid int32, label *Label, filename, logStr string, timespan *uti
 	return count, nil
 }
 
-func importText(tsid int32, label *Label, filename, logStr string, timespan *utils.TimeSpan, pool *pgxpool.Pool) (int64, error) {
+func importText(tsid int32, label *Label, filename, logStr string, pool *pgxpool.Pool) (int64, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		slog.Error(logStr + err.Error())
@@ -85,7 +84,7 @@ func importText(tsid int32, label *Label, filename, logStr string, timespan *uti
 	scanner.Scan()
 
 	if label.IsMetarCloudType() {
-		data, err := parseMetarCloudType(tsid, rowCount, timespan, scanner)
+		data, err := parseMetarCloudType(tsid, rowCount, scanner)
 		if err != nil {
 			slog.Error(logStr + err.Error())
 			return 0, err
@@ -99,7 +98,7 @@ func importText(tsid int32, label *Label, filename, logStr string, timespan *uti
 		return count, nil
 	}
 
-	text, err := parseTextCSV(tsid, rowCount, timespan, scanner)
+	text, err := parseTextCSV(tsid, rowCount, scanner)
 	if err != nil {
 		slog.Error(logStr + err.Error())
 		return 0, err

migrations/kvalobs/db/table.go
@@ -8,8 +8,8 @@ import (
 
 // Maps to `data` and `text_data` tables in Kvalobs
 type Table struct {
-	Name       string
-	Path       string        // Path of the dumped table
+	Name string
+	// Path       string        // Path of the dumped table
 	DumpLabels LabelDumpFunc // Function that dumps labels from the table
 	DumpSeries ObsDumpFunc   // Function that dumps observations from the table
 	Import     ImportFunc    // Function that parses dumps and ingests observations into LARD
@@ -22,4 +22,4 @@ type LabelDumpFunc func(timespan *utils.TimeSpan, pool *pgxpool.Pool, maxConn in
 type ObsDumpFunc func(label *Label, timespan *utils.TimeSpan, path string, pool *pgxpool.Pool) error
 
 // Lard Import function
-type ImportFunc func(tsid int32, label *Label, filename, logStr string, timespan *utils.TimeSpan, pool *pgxpool.Pool) (int64, error)
+type ImportFunc func(tsid int32, label *Label, filename, logStr string, pool *pgxpool.Pool) (int64, error)

metno/lard

migrations/kvalobs/db/csv_parsers.go
@@ -3,14 +3,13 @@ package db
 import (
 	"bufio"
 	"migrate/lard"
-	"migrate/utils"
 	"slices"
 	"strconv"
 	"strings"
 	"time"
 )
 
-func parseDataCSV(tsid int64, rowCount int, timespan *utils.TimeSpan, scanner *bufio.Scanner) ([][]any, [][]any, error) {
+func parseDataCSV(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, [][]any, error) {
 	data := make([][]any, 0, rowCount)
 	flags := make([][]any, 0, rowCount)
 	var originalPtr, correctedPtr *float64
@@ -24,14 +23,7 @@ func parseDataCSV(tsid int64, rowCount int, timespan *utils.TimeSpan, scanner *b
 			return nil, nil, err
 		}
 
-		if timespan.From != nil && obstime.Sub(*timespan.From) < 0 {
-			continue
-		}
-		if timespan.To != nil && obstime.Sub(*timespan.To) > 0 {
-			break
-		}
-
-		original, err := strconv.ParseFloat(fields[1], 64)
+		original, err := strconv.ParseFloat(fields[1], 32)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -79,7 +71,7 @@ func parseDataCSV(tsid int64, rowCount int, timespan *utils.TimeSpan, scanner *b
 }
 
 // Text obs are not flagged
-func parseTextCSV(tsid int64, rowCount int, timespan *utils.TimeSpan, scanner *bufio.Scanner) ([][]any, error) {
+func parseTextCSV(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime
@@ -88,13 +80,6 @@ func parseTextCSV(tsid int64, rowCount int, timespan *utils.TimeSpan, scanner *b
 		obstime, err := time.Parse(time.RFC3339, fields[0])
 		if err != nil {
 			return nil, err
-		}
-
-		if timespan.From != nil && obstime.Sub(*timespan.From) < 0 {
-			continue
-		}
-		if timespan.To != nil && obstime.Sub(*timespan.To) > 0 {
-			break
 		}
 
 		lardObs := lard.TextObs{
@@ -113,7 +98,7 @@ func parseTextCSV(tsid int64, rowCount int, timespan *utils.TimeSpan, scanner *b
 // but should instead be treated as scalars
 // TODO: I'm not sure these params should be scalars given that the other cloud types are not.
 // Should all cloud types be integers or text?
-func parseMetarCloudType(tsid int64, rowCount int, timespan *utils.TimeSpan, scanner *bufio.Scanner) ([][]any, error) {
+func parseMetarCloudType(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime
@@ -122,13 +107,6 @@ func parseMetarCloudType(tsid int64, rowCount int, timespan *utils.TimeSpan, sca
 		obstime, err := time.Parse(time.RFC3339, fields[0])
 		if err != nil {
 			return nil, err
-		}
-
-		if timespan.From != nil && obstime.Sub(*timespan.From) < 0 {
-			continue
-		}
-		if timespan.To != nil && obstime.Sub(*timespan.To) > 0 {
-			break
 		}
 
 		original, err := strconv.ParseFloat(fields[1], 64)
@@ -152,7 +130,7 @@ func parseMetarCloudType(tsid int64, rowCount int, timespan *utils.TimeSpan, sca
 
 // Function for paramids 305, 306, 307, 308 that were stored as scalar data
 // but should be treated as text
-func parseSpecialCloudType(tsid int64, rowCount int, timespan *utils.TimeSpan, scanner *bufio.Scanner) ([][]any, error) {
+func parseSpecialCloudType(tsid int64, rowCount int, scanner *bufio.Scanner) ([][]any, error) {
 	data := make([][]any, 0, rowCount)
 	for scanner.Scan() {
 		// obstime, original, tbtime, corrected, controlinfo, useinfo, cfailed
@@ -162,13 +140,6 @@ func parseSpecialCloudType(tsid int64, rowCount int, timespan *utils.TimeSpan, s
 		obstime, err := time.Parse(time.RFC3339, fields[0])
 		if err != nil {
 			return nil, err
-		}
-
-		if timespan.From != nil && obstime.Sub(*timespan.From) < 0 {
-			continue
-		}
-		if timespan.To != nil && obstime.Sub(*timespan.To) > 0 {
-			break
 		}
 
 		lardObs := lard.TextObs{

migrations/kvalobs/db/import_functions.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"log/slog"
 	"migrate/lard"
-	"migrate/utils"
 	"os"
 	"strconv"
 
@@ -19,7 +18,7 @@ import (
 // - only for histkvalobs
 //      - 2751, 2752, 2753, 2754 are in `text_data` but should be treated as `data`?
 
-func importData(tsid int64, label *Label, filename, logStr string, timespan *utils.TimeSpan, pool *pgxpool.Pool) (int64, error) {
+func importData(tsid int64, label *Label, filename, logStr string, pool *pgxpool.Pool) (int64, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		slog.Error(logStr + err.Error())
@@ -37,7 +36,7 @@ func importData(tsid int64, label *Label, filename, logStr string, timespan *uti
 	scanner.Scan()
 
 	if label.IsSpecialCloudType() {
-		text, err := parseSpecialCloudType(tsid, rowCount, timespan, scanner)
+		text, err := parseSpecialCloudType(tsid, rowCount, scanner)
 		if err != nil {
 			slog.Error(logStr + err.Error())
 			return 0, err
@@ -52,7 +51,7 @@ func importData(tsid int64, label *Label, filename, logStr string, timespan *uti
 		return count, nil
 	}
 
-	data, flags, err := parseDataCSV(tsid, rowCount, timespan, scanner)
+	data, flags, err := parseDataCSV(tsid, rowCount, scanner)
 	count, err := lard.InsertData(data, pool, logStr)
 	if err != nil {
 		slog.Error(logStr + err.Error())
@@ -67,7 +66,7 @@ func importData(tsid int64, label *Label, filename, logStr string, timespan *uti
 	return count, nil
 }
 
-func importText(tsid int64, label *Label, filename, logStr string, timespan *utils.TimeSpan, pool *pgxpool.Pool) (int64, error) {
+func importText(tsid int64, label *Label, filename, logStr string, pool *pgxpool.Pool) (int64, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		slog.Error(logStr + err.Error())
@@ -85,7 +84,7 @@ func importText(tsid int64, label *Label, filename, logStr string, timespan *uti
 	scanner.Scan()
 
 	if label.IsMetarCloudType() {
-		data, err := parseMetarCloudType(tsid, rowCount, timespan, scanner)
+		data, err := parseMetarCloudType(tsid, rowCount, scanner)
 		if err != nil {
 			slog.Error(logStr + err.Error())
 			return 0, err
@@ -99,7 +98,7 @@ func importText(tsid int64, label *Label, filename, logStr string, timespan *uti
 		return count, nil
 	}
 
-	text, err := parseTextCSV(tsid, rowCount, timespan, scanner)
+	text, err := parseTextCSV(tsid, rowCount, scanner)
 	if err != nil {
 		slog.Error(logStr + err.Error())
 		return 0, err

migrations/kvalobs/db/table.go
@@ -8,8 +8,8 @@ import (
 
 // Maps to `data` and `text_data` tables in Kvalobs
 type Table struct {
-	Name       string
-	Path       string        // Path of the dumped table
+	Name string
+	// Path       string        // Path of the dumped table
 	DumpLabels LabelDumpFunc // Function that dumps labels from the table
 	DumpSeries ObsDumpFunc   // Function that dumps observations from the table
 	Import     ImportFunc    // Function that parses dumps and ingests observations into LARD
@@ -22,4 +22,4 @@ type LabelDumpFunc func(timespan *utils.TimeSpan, pool *pgxpool.Pool, maxConn in
 type ObsDumpFunc func(label *Label, timespan *utils.TimeSpan, path string, pool *pgxpool.Pool) error
 
 // Lard Import function
-type ImportFunc func(tsid int64, label *Label, filename, logStr string, timespan *utils.TimeSpan, pool *pgxpool.Pool) (int64, error)
+type ImportFunc func(tsid int64, label *Label, filename, logStr string, pool *pgxpool.Pool) (int64, error)