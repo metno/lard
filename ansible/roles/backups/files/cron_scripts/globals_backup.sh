491671ea3a2a646228625908297af2e5ca533974
Louise Oram <louiseo@met.no>
2025-04-10 12:00:37 +0200

    push last backup metric to prometheus in shell script

--- ansible/roles/backups/files/cron_scripts/globals_backup.sh
+++ ansible/roles/backups/files/cron_scripts/globals_backup.sh
@@ -2,4 +2,7 @@
 
 if repmgr node check --role | grep "primary"; then
     sudo -u postgres pg_dumpall --globals-only | gzip -9 -c | s3cmd put - s3://lard/backups/"globals_$(date +%Y%m%d%H%M%S).sql.gz"
+    # push metrics to both a and b gateways
+    echo "last_globals_backup $(date +%s)" | curl --data-binary @- https://monitoring-a.met.no/obsklim/pushgateway/metrics/job/lard_backup
+    echo "last_globals_backup $(date +%s)" | curl --data-binary @- https://monitoring-b.met.no/obsklim/pushgateway/metrics/job/lard_backup
 fi
