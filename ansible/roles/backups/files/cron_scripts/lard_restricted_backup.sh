128f51630ccd41db6c26734fc31b9bbb5f3b42df
Louise Oram <louiseo@met.no>
2025-04-11 15:32:06 +0200

    create basic staging with ansible

--- ansible/roles/backups/files/cron_scripts/lard_restricted_backup.sh
+++ ansible/roles/backups/files/cron_scripts/lard_restricted_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if repmgr node check --role | grep "primary"; then
-    sudo -u postgres pg_dump lard_restricted | gzip -9 -c | s3cmd put - s3://lard/backups/"lardrestricted_$(date +%Y%m%d%H%M%S).sql.gz"
+    sudo -u postgres pg_dump lard_restricted --compress=gzip | s3cmd put - s3://lard/backups/"lard_restricted_$(date +%Y%m%d%H%M%S).sql.gz"
     # push metrics to both a and b gateways
     echo "last_lard_restricted_backup $(date +%s)" | curl --data-binary @- https://monitoring-a.met.no/obsklim/pushgateway/metrics/job/lard_backup
     echo "last_lard_restricted_backup $(date +%s)" | curl --data-binary @- https://monitoring-b.met.no/obsklim/pushgateway/metrics/job/lard_backup
