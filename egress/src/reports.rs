b6b569d2eca6a6261c88d45bc6f4945b7c11e3d3
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-05-19 11:11:00 +0200

    Un-nest router

--- egress/src/reports.rs
+++ egress/src/reports.rs
@@ -1,11 +1,13 @@
-use axum::Router;
+use axum::{routing::get, Router};
 
 use crate::EgressState;
 
 mod idf_station;
-use idf_station::idf_station_router;
+use idf_station::{idf_station_availability_handler, idf_station_handler};
 pub use idf_station::{IdfMetadata, IdfStationAvailability, IdfStationResp, IdfUnit, IdfValue};
 
-pub fn reports_routes() -> Router<EgressState> {
-    Router::new().nest("/idf", idf_station_router())
+pub fn reports_router() -> Router<EgressState> {
+    Router::new()
+        .route("/idf/station", get(idf_station_availability_handler))
+        .route("/idf/station/{station_id}", get(idf_station_handler))
 }
