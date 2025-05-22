f004a2dde4666428208526092b03ed2004269b72
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-05-22 15:34:07 +0200

    Minor fixes to migrations

--- migrations/lard/index/drop.go
+++ migrations/lard/index/drop.go
@@ -21,8 +21,7 @@ func findIndices(ctx context.Context, pool *pgxpool.Pool) ([]PgIndex, error) {
 	rows, err := pool.Query(
 		ctx,
 		`SELECT schemaname, indexname fROM pg_indexes
-            WHERE schemaname IN ('public', 'legacy')
-			AND tablename IN ('data', 'nonscalar_data')
+            WHERE schemaname IN ('public', 'legacy', 'flags')
             AND NOT indexdef LIKE '%UNIQUE%'`,
 	)
 	if err != nil {
