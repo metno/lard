951a66db7b50571ff22653c67e5d8cbe61d416e5
Ingrid <git@ingrids.email>
2025-05-26 20:31:45 +0200

    integration_tests: test for legacy::raw

--- integration_tests/tests/legacy.rs
+++ integration_tests/tests/legacy.rs
@@ -1,12 +1,13 @@
 use std::{panic::AssertUnwindSafe, time::Instant};
 
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, Duration, TimeZone, Utc};
 use futures::FutureExt;
 use rdkafka::producer::{FutureProducer, FutureRecord};
 
 use lard_ingestion::{get_conversions, DbPools};
 
 pub mod common;
+use common::{Param, TestData};
 
 const KAFKA_RAW_TOPIC: &str = "raw";
 const KAFKA_CHECKED_TOPIC: &str = "checked";
@@ -69,7 +70,7 @@ pub async fn e2e_test_wrapper_legacy(test: impl AsyncFnOnce(FutureProducer, DbPo
 }
 
 #[tokio::test]
-async fn test_kafka() {
+async fn test_kafka_checked() {
     e2e_test_wrapper_legacy(async |producer: FutureProducer, db_pools: DbPools| {
         // This observation was 2.5 hours late??
         let kafka_xml = r#"<?xml?>
@@ -210,6 +211,107 @@ async fn test_kafka() {
                 assert_eq!(station_id, Some(20001));
                 assert_eq!(param_id, Some(106));
                 assert_eq!(type_id, Some(-4));
+                assert_eq!(lvl, Some(0));
+                assert_eq!(sensor, Some(0));
+
+                break;
+            }
+
+            if timeout_start.elapsed() > timeout {
+                panic!("Timed out waiting for data to appear")
+            }
+        }
+    })
+    .await
+}
+
+#[tokio::test]
+async fn test_kafka_raw() {
+    e2e_test_wrapper_legacy(async |producer: FutureProducer, db_pools: DbPools| {
+        let ts = TestData {
+            station_id: 20001,
+            params: vec![Param::new("TA")],
+            // start_time: Utc::now().duration_trunc(TimeDelta::hours(1)).unwrap()
+            //     - Duration::hours(11),
+            start_time: Utc.with_ymd_and_hms(2024, 6, 6, 6, 0, 0).unwrap(),
+            period: Duration::hours(1),
+            type_id: 501,
+            len: 1,
+        };
+
+        producer
+            .send_result(
+                FutureRecord::to(KAFKA_RAW_TOPIC)
+                    .key("")
+                    .payload(&ts.obsinn_message()),
+            )
+            .unwrap()
+            .await
+            .unwrap()
+            .unwrap();
+
+        // TODO: we do not have an API endpoint to query the flags.kvdata table
+        let open_conn = db_pools.open.get().await.unwrap();
+
+        // As we have no way to sync with message processing in kvkafka ingestion, we just keep
+        // trying to fetch data with a timeout
+        let timeout = std::time::Duration::from_secs(10);
+        let timeout_start = Instant::now();
+        loop {
+            if let Ok(data_row) = open_conn
+                .query_one(
+                    r#"
+                        SELECT
+                            timeseries,
+                            obstime,
+                            original
+                        FROM legacy.data
+                    "#,
+                    &[],
+                )
+                .await
+            {
+                let (timeseries, obstime, original): (i64, DateTime<Utc>, Option<f64>) =
+                    (data_row.get(0), data_row.get(1), data_row.get(2));
+                assert_eq!(obstime, Utc.with_ymd_and_hms(2024, 6, 6, 6, 0, 0).unwrap());
+                assert_eq!(original, Some(0.));
+
+                let label_row = open_conn
+                    .query_one(
+                        r#"
+                        SELECT
+                            station_id,
+                            param_id,
+                            type_id,
+                            lvl,
+                            sensor
+                        FROM labels.kvalobs
+                        WHERE timeseries = $1
+                    "#,
+                        &[&timeseries],
+                    )
+                    .await
+                    .unwrap();
+
+                #[allow(clippy::type_complexity)]
+                let (station_id, param_id, type_id, lvl, sensor): (
+                    // should these really all be Option??
+                    Option<i32>,
+                    Option<i32>,
+                    Option<i32>,
+                    Option<i32>,
+                    Option<i32>,
+                ) = (
+                    label_row.get(0),
+                    label_row.get(1),
+                    label_row.get(2),
+                    label_row.get(3),
+                    label_row.get(4),
+                );
+
+                assert_eq!(station_id, Some(20001));
+                assert_eq!(param_id, Some(211));
+                assert_eq!(type_id, Some(501));
                 assert_eq!(lvl, Some(0));
                 assert_eq!(sensor, Some(0));
 
