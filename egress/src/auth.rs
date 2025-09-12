e4ff859be1b34056b0e3123fc78d51aaf87b8fce
Louise Oram <louiseo@met.no>
2025-09-12 11:17:54 +0200

    Apply suggestions from code review
    Co-authored-by: Manuel Carrer <manuel.carrer@zohomail.eu>

--- egress/src/auth.rs
+++ egress/src/auth.rs
@@ -64,33 +64,26 @@ pub async fn cache_jwks_certs() -> Result<JWKScerts, Error> {
 }
 
 fn parse_permitid(roles: Vec<String>) -> Vec<i32> {
-    let mut permit_list: Vec<i32> = Vec::new();
     // find the numbers after the string permitid
     let re = Regex::new(r".*?permitid-(\d+)").unwrap();
-    for x in roles {
-        if let Some(captures) = re.captures(&x) {
-            if let Some(end_num) = captures.get(1) {
-                let permit = end_num.as_str().parse::<i32>();
-                if let Ok(p) = permit {
-                    permit_list.push(p);
-                }
-            }
-        }
-    }
-    permit_list
+
+    roles
+        .iter()
+        .filter_map(|role| re.captures(role))
+        .filter_map(|capture| capture.get(0))
+        .filter_map(|end_num| end_num.as_str().parse::<i32>().ok())
+        .collect()
 }
 
 // verify a token with the certs
 pub async fn verify_token(token_str: &str, certs: JWKScerts) -> Result<Vec<i32>, Error> {
     let mut validation = Validation::new(Algorithm::ES384);
     validation.set_audience(&["ODA"]);
-    let token_message = decode::<Claims>(token_str, &certs, &validation);
-    if let Ok(tm) = token_message {
-        Ok(parse_permitid(tm.claims.resource_access.resource.roles))
-    } else {
-        let token_message_err = token_message.unwrap_err();
-        Err(Error::Auth(token_message_err.to_string()))
-    }
+    let token_message = decode::<Claims>(token_str, &certs, &validation)?;
+
+    Ok(parse_permitid(
+        token_message.claims.resource_access.resource.roles,
+    ))
 }
 
 async fn parse_auth_header(header: &str) -> Option<String> {
@@ -118,19 +111,17 @@ pub async fn auth_middleware(
         }
     };
 
-    if let Some(token) = parse_auth_header(auth_header).await {
-        let roles = verify_token(&token, certs).await;
-        // insert the roles into a request extension so the handler can extract it
-        if let Ok(r) = roles {
-            req.extensions_mut().insert(Some(r));
-            Ok(next.run(req).await)
-        } else {
-            // token could not be verified
-            Err(StatusCode::UNAUTHORIZED)
-        }
-    } else {
+    let token = parse_auth_header(auth_header)
+        .await
         // didn't have the expected bearer token format
         // 400 includes "malformed request syntax, invalid request message framing"
-        Err(StatusCode::BAD_REQUEST)
-    }
+        .ok_or(StatusCode::BAD_REQUEST)?;
+
+    let roles = verify_token(&token, certs)
+        .await
+        // token could not be verified
+        .map_err(|_| StatusCode::UNAUTHORIZED)?;
+
+    req.extensions_mut().insert(Some(roles));
+    Ok(next.run(req).await)
 }
