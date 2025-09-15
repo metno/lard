fd0523b6902124ca6f73d5a4039e49e9b22702d6
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-09-15 15:34:46 +0200

    Revert reports router name change

--- egress/src/reports.rs
+++ egress/src/reports.rs
@@ -10,7 +10,7 @@ mod idf_station;
 use idf_station::{idf_station_availability_handler, idf_station_handler};
 pub use idf_station::{IdfMetadata, IdfStationAvailability, IdfStationResp, IdfUnit, IdfValue};
 
-pub fn set_routes() -> Router<EgressState> {
+pub fn reports_router() -> Router<EgressState> {
     Router::new()
         .route("/idf/station", get(idf_station_availability_handler))
         .route("/idf/station/{station_id}", get(idf_station_handler))
