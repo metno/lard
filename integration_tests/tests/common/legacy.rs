9d91d11b1cc3bf7243f5121e6f38f8d91b0b0cf0
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-09-18 17:16:34 +0200

    Split integration tests in separate unit files

---
+++ integration_tests/tests/common/legacy.rs
@@ -0,0 +1,168 @@
+use std::{panic::AssertUnwindSafe, time::Instant};
+
+use futures::FutureExt;
+use lard_egress::patchwork::PatchworkTables;
+use lard_ingestion::{get_conversions, util::permissions::timeseries_get_permit};
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use util::{DbPools, PooledPgConn};
+
+#[cfg(not(feature = "debug"))]
+use crate::common::db_cleanup;
+
+use crate::common::{
+    mock_level_table, mock_permit_tables, update_patchwork_table, wrapper_setup, TestData,
+};
+
+pub const KAFKA_CHECKED_TOPIC: &str = "checked";
+const KAFKA_RAW_TOPIC: &str = "raw";
+const KAFKA_CHECKED_HIST_TOPIC: &str = "hist.checked";
+const KAFKA_GROUP: &str = "lard_test";
+
+pub struct IngestData<'a> {
+    timeseries: Vec<TestData<'a>>,
+    expected_open: usize,
+    expected_restricted: usize,
+}
+
+impl<'a> IngestData<'a> {
+    pub fn new(data: Vec<TestData<'a>>) -> Self {
+        let mut expected_open = 0;
+        let mut expected_restricted = 0;
+
+        // Calculate expected rows to be found in the database after ingestion
+        // To be honest this feels like another hack
+        for ts in &data {
+            for param in &ts.params {
+                let permit = timeseries_get_permit(
+                    mock_permit_tables(),
+                    ts.station_id,
+                    ts.type_id,
+                    Some(param.id),
+                )
+                .unwrap();
+                if permit == Some(1) {
+                    expected_open += ts.len;
+                } else {
+                    expected_restricted += ts.len
+                }
+            }
+        }
+
+        Self {
+            timeseries: data,
+            expected_open,
+            expected_restricted,
+        }
+    }
+}
+
+// Helper function that waits for data to be available
+pub async fn wait_for_db_readiness(conn: &PooledPgConn<'_>, expected_rows: usize) {
+    let timeout = std::time::Duration::from_secs(10);
+    let timeout_start = Instant::now();
+    loop {
+        if let Ok(rows) = conn.query("SELECT timeseries FROM legacy.data", &[]).await {
+            if rows.len() == expected_rows {
+                break;
+            }
+        };
+
+        if timeout_start.elapsed() > timeout {
+            panic!("Timed out waiting for data to appear")
+        }
+    }
+}
+
+/// Helper function that ingests data into the raw queue, waits for it to be available, and updates
+/// the patchwork tables
+pub async fn ingest_raw(
+    data: &IngestData<'_>,
+    producer: FutureProducer,
+    pools: DbPools,
+    tables: PatchworkTables,
+) {
+    for ts in &data.timeseries {
+        producer
+            .send_result(
+                FutureRecord::to(KAFKA_RAW_TOPIC)
+                    .key("")
+                    .payload(&ts.obsinn_zeros()),
+            )
+            .unwrap()
+            .await
+            .unwrap()
+            .unwrap();
+    }
+
+    let open_conn = pools.open.get().await.unwrap();
+    let restricted_conn = pools.restricted.get().await.unwrap();
+
+    // As we have no way to sync with message processing in kvkafka ingestion, we just keep
+    // trying to fetch data with a timeout
+    tokio::join!(
+        wait_for_db_readiness(&open_conn, data.expected_open),
+        wait_for_db_readiness(&restricted_conn, data.expected_restricted),
+    );
+
+    tokio::join!(
+        update_patchwork_table(&open_conn, tables.open),
+        update_patchwork_table(&restricted_conn, tables.restricted)
+    );
+}
+
+/// Similar to e2e_test_wrapper, but adapted to use kvkafka ingestion instead of obsinn.
+pub async fn e2e_test_wrapper_legacy(
+    test: impl AsyncFnOnce(FutureProducer, DbPools, PatchworkTables) -> (),
+) {
+    let (db_pools, patchwork_tables, mut egress, cancel_token) = wrapper_setup().await;
+
+    let mock_kafka_cluster = rdkafka::mocking::MockCluster::new(3).unwrap();
+    mock_kafka_cluster
+        .create_topic(KAFKA_RAW_TOPIC, 32, 3)
+        .unwrap();
+    mock_kafka_cluster
+        .create_topic(KAFKA_CHECKED_TOPIC, 32, 3)
+        .unwrap();
+    let kafka_brokers = mock_kafka_cluster.bootstrap_servers();
+
+    let kafka_producer: FutureProducer = rdkafka::ClientConfig::new()
+        .set("bootstrap.servers", kafka_brokers.clone())
+        .create()
+        .unwrap();
+
+    let param_conv_path = std::env::var("PARAMCONV_CSV").unwrap();
+    let param_conversions =
+        get_conversions(&param_conv_path).expect("failed to load param conversions");
+
+    let (ingestion_pools, ingestion_token) = (db_pools.clone(), cancel_token.clone());
+    let mut ingestion = tokio::spawn(lard_ingestion::legacy::run(
+        ingestion_pools,
+        kafka_brokers,
+        KAFKA_GROUP.to_string(),
+        KAFKA_RAW_TOPIC,
+        KAFKA_CHECKED_TOPIC,
+        KAFKA_CHECKED_HIST_TOPIC,
+        ingestion_token,
+        mock_permit_tables(),
+        mock_level_table(),
+        param_conversions,
+    ));
+
+    tokio::select! {
+        _ = &mut egress => panic!("API server task terminated first"),
+        _ = &mut ingestion => panic!("Ingestor server task terminated first"),
+        // Clean up database even if test panics, to avoid test poisoning
+        test_result = AssertUnwindSafe(test(kafka_producer, db_pools.clone(), patchwork_tables.clone())).catch_unwind() => {
+            // For debugging a specific test, it might be useful to skip the cleanup process
+            #[cfg(not(feature = "debug"))]
+            db_cleanup(db_pools.clone()).await;
+
+            assert!(test_result.is_ok())
+        }
+    }
+
+    cancel_token.cancel();
+    let (egress_result, ingestion_result) = tokio::join!(egress, ingestion);
+    egress_result.unwrap();
+    ingestion_result.unwrap().unwrap();
+}
---
+++ integration_tests/tests/idf_event.rs
@@ -0,0 +1,453 @@
+use chrono::{DateTime, Duration, TimeZone, Utc};
+use lard_egress::reports::IdfEventAvailable;
+use rdkafka::producer::FutureProducer;
+use reqwest::Client;
+
+use lard_egress::{
+    patchwork::PatchworkTables,
+    reports::{IdfEvent, IdfEventAvailabilityResp, IdfEventResp, DEFAULT_DURATIONS},
+};
+
+use util::DbPools;
+
+pub mod common;
+use common::{
+    legacy::{e2e_test_wrapper_legacy, ingest_raw, IngestData},
+    Param, TestData, RESTRICTED_TOKEN,
+};
+
+#[tokio::test]
+async fn test_idf_event_availability() {
+    // Message priority default times
+    let priority_switch: DateTime<Utc> = Utc.with_ymd_and_hms(2025, 1, 1, 0, 0, 0).unwrap();
+
+    // Timeseries start time
+    let start_time = Utc.with_ymd_and_hms(2024, 12, 31, 23, 50, 0).unwrap();
+    let cases = [
+        (
+            Some(RESTRICTED_TOKEN),
+            IdfEventAvailabilityResp {
+                stations: vec![
+                    IdfEventAvailable::new(10001, 1, start_time, Some(priority_switch)),
+                    IdfEventAvailable::new(20001, 1, priority_switch, None),
+                    IdfEventAvailable::new(99995, 5, priority_switch, None),
+                ],
+            },
+        ),
+        (
+            None,
+            IdfEventAvailabilityResp {
+                stations: vec![
+                    IdfEventAvailable::new(10001, 1, start_time, Some(priority_switch)),
+                    IdfEventAvailable::new(20001, 1, priority_switch, None),
+                ],
+            },
+        ),
+    ];
+
+    e2e_test_wrapper_legacy(
+        async |producer: FutureProducer, db_pools: DbPools, tables: PatchworkTables| {
+            let test_data = IngestData::new(vec![
+                TestData {
+                    station_id: 10001,
+                    params: vec![Param::new("RR_01")],
+                    start_time,
+                    period: Duration::minutes(1),
+                    type_id: 514,
+                    len: 20,
+                },
+                TestData {
+                    station_id: 20001,
+                    params: vec![Param::new("RR_01")],
+                    start_time,
+                    period: Duration::minutes(1),
+                    type_id: 508,
+                    len: 20,
+                },
+                TestData {
+                    station_id: 99995,
+                    params: vec![Param::new("RR_01")],
+                    start_time,
+                    period: Duration::minutes(1),
+                    type_id: 508,
+                    len: 20,
+                },
+            ]);
+
+            ingest_raw(&test_data, producer, db_pools, tables).await;
+
+            let url = "http://localhost:3000/reports/idf/event";
+
+            for (token, expected) in cases {
+                let client = Client::new();
+                let request = match token {
+                    Some(t) => client.get(url).bearer_auth(t),
+                    None => client.get(url),
+                };
+
+                let resp = request.send().await.unwrap();
+                assert!(resp.status().is_success(), "{}", resp.text().await.unwrap());
+
+                let mut json: IdfEventAvailabilityResp = resp.json().await.unwrap();
+
+                // Sort so that the stations are in order
+                json.stations.sort_by_key(|s| s.station_id);
+
+                assert_eq!(json, expected);
+            }
+        },
+    )
+    .await
+}
+
+#[tokio::test]
+async fn test_idf_event() {
+    let start_time = Utc.with_ymd_and_hms(2024, 12, 31, 23, 40, 0).unwrap();
+    let end_first_ts = Utc.with_ymd_and_hms(2024, 12, 31, 23, 49, 0).unwrap();
+    let end_second_ts = Utc.with_ymd_and_hms(2025, 1, 1, 0, 9, 0).unwrap();
+
+    let station_id = 10001;
+    let test_data = IngestData::new(vec![
+        TestData {
+            station_id,
+            params: vec![Param::new("RR_01")],
+            start_time,
+            period: Duration::minutes(1),
+            type_id: 514,
+            len: 30,
+        },
+        TestData {
+            station_id,
+            params: vec![Param::new("RR_01")],
+            start_time,
+            period: Duration::minutes(1),
+            type_id: 508,
+            len: 30,
+        },
+    ]);
+
+    let cases = [
+        (
+            // Only extract 2 timestamps for simplicity
+            "default durations",
+            start_time,
+            start_time + Duration::minutes(2),
+            None,
+            // Skip the first element (duration = 1), since that one can only return a single
+            // timestamp
+            vec![IdfEvent::new(1.0, 1, start_time, start_time)]
+                .into_iter()
+                .chain(
+                    // All durations > 1 should return the same intensity and timestamps
+                    DEFAULT_DURATIONS[1..].iter().map(|d| {
+                        IdfEvent::new(2.0, *d, start_time, start_time + Duration::minutes(1))
+                    }),
+                )
+                .collect(),
+        ),
+        (
+            // Should only get the first timeseries
+            "single duration",
+            start_time,
+            start_time + Duration::minutes(10),
+            Some(vec![10]),
+            vec![IdfEvent::new(10.0, 10, start_time, end_first_ts)],
+        ),
+        (
+            // Should get both timeseries
+            "multiple durations",
+            start_time,
+            start_time + Duration::minutes(50),
+            Some(vec![10, 40]),
+            vec![
+                IdfEvent::new(10.0, 10, start_time, end_first_ts),
+                IdfEvent::new(30.0, 40, start_time, end_second_ts),
+            ],
+        ),
+    ];
+
+    e2e_test_wrapper_legacy(
+        async |producer: FutureProducer, db_pools: DbPools, tables: PatchworkTables| {
+            ingest_raw(&test_data, producer, db_pools.clone(), tables).await;
+
+            // HACK: need to set corrected and quality_code to be able to compute idf event
+            let open_conn = db_pools.open.get().await.unwrap();
+            open_conn
+                .execute(
+                    "UPDATE legacy.data SET corrected = 1.0, quality_code = 1",
+                    &[],
+                )
+                .await
+                .unwrap();
+
+            for (title, from, to, durations, expected) in cases {
+                let duration_query = durations
+                    .map(|v| {
+                        let joined = v
+                            .iter()
+                            .map(|d| d.to_string())
+                            .collect::<Vec<_>>()
+                            .join(",");
+                        format!("&durations={joined}")
+                    })
+                    .unwrap_or("".to_string());
+
+                let url = format!(
+                    "http://localhost:3000/reports/idf/event/{station_id}\
+                        ?fromtime={from}\
+                        &totime={to}\
+                        {duration_query}",
+                );
+
+                let resp = reqwest::get(url).await.unwrap();
+                assert!(
+                    resp.status().is_success(),
+                    "{title}: {}",
+                    resp.text().await.unwrap()
+                );
+
+                let json: IdfEventResp = resp.json().await.unwrap();
+                assert_eq!(json.station_id, station_id, "{title}");
+                assert_eq!(json.values.len(), expected.len(), "{title}");
+
+                for (val, exp) in json.values.into_iter().zip(expected) {
+                    assert_eq!(val, exp, "{title}")
+                }
+            }
+        },
+    )
+    .await
+}
+
+#[tokio::test]
+async fn test_idf_event_failure() {
+    let start_time = Utc.with_ymd_and_hms(2024, 12, 31, 23, 40, 0).unwrap();
+    let end_time = Utc.with_ymd_and_hms(2025, 1, 1, 0, 9, 0).unwrap();
+
+    let station_id = 10001;
+    let test_data = IngestData::new(vec![
+        TestData {
+            station_id,
+            params: vec![Param::new("RR_01")],
+            start_time,
+            period: Duration::minutes(1),
+            type_id: 514,
+            len: 30,
+        },
+        TestData {
+            station_id,
+            params: vec![Param::new("RR_01")],
+            start_time,
+            period: Duration::minutes(1),
+            type_id: 508,
+            len: 30,
+        },
+    ]);
+
+    e2e_test_wrapper_legacy(
+        async |producer: FutureProducer, db_pools: DbPools, tables: PatchworkTables| {
+            ingest_raw(&test_data, producer, db_pools, tables).await;
+
+            let url = format!(
+                "http://localhost:3000/reports/idf/event/{station_id}\
+                        ?fromtime={start_time}\
+                        &totime={end_time}"
+            );
+
+            let resp = reqwest::get(url).await.unwrap();
+
+            // Since the data is not QCed we won't get a positive response (not found error)
+            assert!(
+                resp.status().is_client_error(),
+                "{}",
+                resp.text().await.unwrap()
+            );
+        },
+    )
+    .await
+}
+
+#[tokio::test]
+async fn test_idf_event_restricted() {
+    let start_time = Utc.with_ymd_and_hms(2024, 12, 31, 23, 40, 0).unwrap();
+    let station_id = 99995;
+
+    let test_data = IngestData::new(vec![
+        TestData {
+            station_id,
+            params: vec![Param::new("RR_01")],
+            start_time,
+            period: Duration::minutes(1),
+            type_id: 514,
+            len: 30,
+        },
+        TestData {
+            station_id,
+            params: vec![Param::new("RR_01")],
+            start_time,
+            period: Duration::minutes(1),
+            type_id: 508,
+            len: 30,
+        },
+    ]);
+
+    let cases = [(
+        // Should only get the first timeseries
+        "single duration",
+        start_time,
+        start_time + Duration::minutes(10),
+        "10",
+        vec![IdfEvent::new(
+            10.0,
+            10,
+            start_time,
+            start_time + Duration::minutes(9),
+        )],
+    )];
+
+    e2e_test_wrapper_legacy(
+        async |producer: FutureProducer, db_pools: DbPools, tables: PatchworkTables| {
+            ingest_raw(&test_data, producer, db_pools.clone(), tables).await;
+
+            // HACK: need to set corrected and quality_code to be able to compute idf event
+            let conn = db_pools.restricted.get().await.unwrap();
+            conn.execute(
+                "UPDATE legacy.data SET corrected = 1.0, quality_code = 1",
+                &[],
+            )
+            .await
+            .unwrap();
+
+            for (title, from, to, duration, expected) in cases {
+                let url = format!(
+                    "http://localhost:3000/reports/idf/event/{station_id}\
+                    ?fromtime={from}\
+                    &totime={to}\
+                    &durations={duration}",
+                );
+
+                let resp = Client::new()
+                    .get(url)
+                    .bearer_auth(RESTRICTED_TOKEN)
+                    .send()
+                    .await
+                    .unwrap();
+                assert!(
+                    resp.status().is_success(),
+                    "{title}: {}",
+                    resp.text().await.unwrap()
+                );
+
+                let json: IdfEventResp = resp.json().await.unwrap();
+                assert_eq!(json.station_id, station_id, "{title}");
+                assert_eq!(json.values.len(), expected.len(), "{title}");
+
+                for (val, exp) in json.values.into_iter().zip(expected) {
+                    assert_eq!(val, exp, "{title}")
+                }
+            }
+        },
+    )
+    .await
+}
+
+// NOTE: this test will fail if the patches in the patchwork table are not sorted
+#[tokio::test]
+async fn test_idf_event_sorted() {
+    let start_time = Utc.with_ymd_and_hms(2024, 12, 31, 22, 50, 0).unwrap();
+    let priority_shift_time = Utc.with_ymd_and_hms(2025, 1, 1, 0, 0, 0).unwrap();
+    let station_id = 10001;
+
+    let test_data = IngestData::new(vec![
+        TestData {
+            station_id,
+            params: vec![Param::new("RR_01")],
+            start_time,
+            period: Duration::minutes(1),
+            type_id: 514,
+            len: 80,
+        },
+        TestData {
+            station_id,
+            params: vec![Param::new("RR_01")],
+            start_time,
+            period: Duration::minutes(1),
+            type_id: 508,
+            len: 80,
+        },
+        TestData {
+            station_id,
+            params: vec![Param::new("RR_01")],
+            start_time,
+            period: Duration::minutes(1),
+            type_id: 501,
+            len: 80,
+        },
+    ]);
+
+    let cases = [(
+        "single duration",
+        priority_shift_time - Duration::minutes(3),
+        priority_shift_time + Duration::minutes(1),
+        "4",
+        vec![IdfEvent::new(
+            // We have three timeseries in three different patches (delimited by |)
+            // | 1 1 1 ... | ... 1 \ 1 1 1 | 2 \ ... |
+            // We are asking data in the interval delimited by \
+            // If the patches are not sorted, the sum accumulation would stop before the first
+            // observation with value 2
+            5.0,
+            4,
+            priority_shift_time - Duration::minutes(3),
+            priority_shift_time,
+        )],
+    )];
+
+    e2e_test_wrapper_legacy(
+        async |producer: FutureProducer, db_pools: DbPools, tables: PatchworkTables| {
+            ingest_raw(&test_data, producer, db_pools.clone(), tables).await;
+
+            // HACK: need to set corrected and quality_code to be able to compute idf event
+            let conn = db_pools.open.get().await.unwrap();
+            conn.execute(
+                "UPDATE legacy.data SET corrected = 1.0, quality_code = 1 WHERE obstime < $1",
+                &[&priority_shift_time],
+            )
+            .await
+            .unwrap();
+
+            // Set a different value for the second timeseries
+            conn.execute(
+                "UPDATE legacy.data SET corrected = 2.0, quality_code = 1 WHERE obstime >= $1",
+                &[&priority_shift_time],
+            )
+            .await
+            .unwrap();
+
+            for (title, from, to, duration, expected) in cases {
+                let url = format!(
+                    "http://localhost:3000/reports/idf/event/{station_id}\
+                    ?fromtime={from}\
+                    &totime={to}\
+                    &durations={duration}",
+                );
+
+                let resp = Client::new().get(url).send().await.unwrap();
+                assert!(
+                    resp.status().is_success(),
+                    "{title}: {}",
+                    resp.text().await.unwrap()
+                );
+
+                let json: IdfEventResp = resp.json().await.unwrap();
+                assert_eq!(json.station_id, station_id, "{title}");
+                assert_eq!(json.values.len(), expected.len(), "{title}");
+
+                for (val, exp) in json.values.into_iter().zip(expected) {
+                    assert_eq!(val, exp, "{title}")
+                }
+            }
+        },
+    )
+    .await
+}
