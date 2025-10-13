c59a0f1ee3184e269900c48d1e739e5a53049750
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-10-13 11:17:25 +0200

    Add Cron struct so we can define intervals outside of the cron functions

--- ingestion/src/cron.rs
+++ ingestion/src/cron.rs
@@ -1,3 +1,4 @@
+use tokio::time::Interval;
 use tracing::{error, info};
 use util::DbPools;
 
@@ -8,15 +9,16 @@ use crate::util::{
     tsupdate::{self},
 };
 
-const HOUR: u64 = 3600;
+pub struct Cron<State> {
+    pub state: State,
+    pub interval: Interval,
+}
 
 // TODO: refactor how these two tables are refreshed, since could be more elegantly combined
 // (especially if have more tables in the future)
-pub async fn refresh_permits(stinfo_conn_string: String, background_permit_tables: PermitTables) {
-    let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(30 * 60));
-
+pub async fn refresh_permits(stinfo_conn_string: String, mut cron: Cron<PermitTables>) {
     loop {
-        interval.tick().await;
+        cron.interval.tick().await;
         info!("Refreshing permit tables");
 
         // TODO: is the async block needed to drop the mutex? Isn't it dropped after each
@@ -28,37 +30,41 @@ pub async fn refresh_permits(stinfo_conn_string: String, background_permit_table
             let new_permit_tables = permissions::fetch_permits(&stinfo_conn_string)
                 .await
                 .unwrap();
-            let mut tables = background_permit_tables.write().unwrap();
+
+            let mut tables = cron.state.write().unwrap();
             *tables = new_permit_tables;
         }
         .await;
     }
 }
 
-pub async fn refresh_levels(stinfo_conn_string: String, background_level_table: LevelTable) {
-    let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(30 * 60));
-
+pub async fn refresh_levels(stinfo_conn_string: String, mut cron: Cron<LevelTable>) {
     loop {
-        interval.tick().await;
+        cron.interval.tick().await;
         info!("Refreshing level tables");
 
         // TODO: is the async block needed to drop the mutex? Isn't it dropped after each
         // iteration?
         async {
             let new_level_table = levels::fetch_levels(&stinfo_conn_string).await.unwrap();
-            let mut tables = background_level_table.write().unwrap();
+            let mut tables = cron.state.write().unwrap();
             *tables = new_level_table;
         }
         .await;
     }
 }
 
-pub async fn refresh_deactivated(stinfo_conn_string: String, levels: LevelTable, pools: DbPools) {
-    let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(12 * HOUR));
+pub async fn refresh_deactivated(
+    stinfo_conn_string: String,
+    mut cron: Cron<(LevelTable, DbPools)>,
+) {
+    let levels = cron.state.0;
+    let pools = cron.state.1;
+
     let stinfosys = Stinfosys::new(stinfo_conn_string, levels);
 
     loop {
-        interval.tick().await;
+        cron.interval.tick().await;
         info!("Updating timeseries totime");
 
         // TODO: add retries instead of panicking?
