453fb1f05daadf1d547be681919bb9bbf65e956d
Manuel Carrer <manuel.carrer@zohomail.eu>
2025-10-14 16:56:28 +0200

    Update migrate_dump.sh

--- migrations/migrate_dump.sh
+++ migrations/migrate_dump.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+go build
+
+# dump data from kvalobs in 2 year windows
 for from_year in {2008..2024..2}; do
-  echo "Current from: $from_year"
-  to_year=$((from_year+2))
-  echo "Current to: $to_year"
-  echo
-  tmux new-session\; send "./migrate kvalobs dump --path /mnt/dumps --from $from_year-01-01 --to $to_year-01-01" ENTER \; detach
+    to_year=$((from_year + 2))
+
+    echo "Dumping kvalobs data from $from_year to $to_year"
+
+    tmux new-session\; send "./migrate kvalobs dump \
+      --path /mnt/dumps \
+      --from $from_year-01-01 \
+      --to $to_year-01-01" ENTER \; detach
+
+    echo
 done
+
+echo "Dumping tables from KDVH"
+tmux new-session\; send "./migrate kdvh dump" ENTER \; detach
