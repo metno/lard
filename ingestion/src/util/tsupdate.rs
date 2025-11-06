8814dc0d123fb38624803bc37e5bce97e0cd2538
Louise Oram <louiseo@met.no>
2025-11-06 13:09:01 +0100

    add sql and function for fixing twisted from/to times

--- ingestion/src/util/tsupdate.rs
+++ ingestion/src/util/tsupdate.rs
@@ -22,6 +22,21 @@ const OPEN_TIMESERIES_QUERY: &str = "\
     WHERE met.param_id IS NOT NULL \
     AND timeseries.totime IS NULL";
 
+// find timeseries where totime is before fromtime
+const TWISTED_TIMESERIES_QUERY: &str = "\
+    SELECT \
+        timeseries.id, \
+        met.station_id, \
+        met.param_id, \
+        met.type_id, \
+        met.lvl, \
+        met.sensor \
+    FROM labels.met \
+    JOIN timeseries \
+        ON met.timeseries = timeseries.id \
+    WHERE met.param_id IS NOT NULL \
+    AND timeseries.totime < timeseries.fromtime";
+
 const UPDATE_QUERY: &str = "\
     UPDATE public.timeseries SET \
         totime = $1, \
@@ -55,6 +70,65 @@ pub async fn set_deactivated(
     .await?;
 
     let rows = tx.query(OPEN_TIMESERIES_QUERY, &[]).await?;
+
+    let labels = rows
+        .iter()
+        .map(|row| {
+            MetLabel::new(
+                row.get(0),
+                row.get(1),
+                row.get(2),
+                row.get(3),
+                row.get(4),
+                row.get(5),
+            )
+        })
+        .collect();
+
+    let deactivated = fetch_deactivated(
+        obs_pgm_totime,
+        obs_pgm_fromtime,
+        station_totime,
+        station_fromtime,
+        labels,
+    )
+    .await?;
+
+    future::join_all(deactivated.into_iter().map(async |ts| {
+        match tx
+            .execute(UPDATE_QUERY, &[&ts.totime, &ts.fromtime, &ts.tsid])
+            .await
+        {
+            Ok(_) => (), //info!("Tsid {} updated", ts.tsid),
+            Err(err) => error!("Could not update tsid {}: {}", ts.tsid, err),
+        }
+    }))
+    .await;
+
+    tx.commit().await?;
+
+    Ok(())
+}
+
+// these were created by the original fetch_deactivated function, since it only set totime
+// need to be found and fixed separately
+pub async fn untwist_timeseries(
+    conn: &mut PooledPgConn<'_>,
+    obs_pgm_totime: &HashMap<MetTimeseriesKey, DateTime<Utc>>,
+    obs_pgm_fromtime: &HashMap<MetTimeseriesKey, DateTime<Utc>>,
+    station_totime: &HashMap<i32, DateTime<Utc>>,
+    station_fromtime: &HashMap<i32, DateTime<Utc>>,
+) -> Result<(), Error> {
+    let tx = conn.transaction().await?;
+
+    // Explicitly take the lock so we can prevent concurrent access to the rows we are going to update
+    tx.execute(
+        "LOCK TABLE public.timeseries IN SHARE ROW EXCLUSIVE MODE",
+        &[],
+    )
+    .await?;
+
+    let rows = tx.query(TWISTED_TIMESERIES_QUERY, &[]).await?;
 
     let labels = rows
         .iter()
