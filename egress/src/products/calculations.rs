26c40b9b17be5b96b60fe83e0b79559c04446689
Louise Carolyn Oram <louiseo@met.no>
2025-12-11 11:55:38 +0100

    use Ketils code for dew_point calculation, refactor the stucture for parsing data

--- egress/src/products/calculations.rs
+++ egress/src/products/calculations.rs
@@ -49,8 +49,33 @@ pub struct CalculationsDatum {
 }
 
 pub fn dew_point_temperature(temperature: f64, relative_humidity: f64) -> Result<f64, Error> {
-    //'$3=$1-((100-$2)/5)'
-    Ok(temperature - ((100.0 - relative_humidity) / 5.0))
+    let vp = calc_vp(temperature, relative_humidity);
+    let td = calc_td(temperature, vp);
+
+    Ok((td * 10.0).round() / 10.0)
+}
+
+pub fn calc_vp(ta: f64, uu: f64) -> f64 {
+    let e = calc_vp_vapor(ta);
+    (uu * e) / 100.0
+}
+
+fn calc_vp_vapor(ta: f64) -> f64 {
+    // Tetens over 0 and Magnus under 0?
+    // possible dirty trick which will look very messy:
+    // (ta+abs(ta))/(2*ta) is +1 for pos and 0 for neg
+    // (ta-abs(ta))/(2*ta) is 0 for pos and +1 for neg
+    if ta > 0.0 {
+        return 6.10780 * (17.08085 * ta / (234.175 + ta)).exp();
+    }
+    6.10780 * (17.84362 * ta / (245.425 + ta)).exp()
+}
+
+pub fn calc_td(ta: f64, vp: f64) -> f64 {
+    if ta > 0.0 {
+        return 245.425 * (vp / 6.10780).ln() / (17.84362 - (vp / 6.10780).ln());
+    }
+    234.175 * (vp / 6.10780).ln() / (17.08085 - (vp / 6.10780).ln())
 }
 
 pub fn get_available_products(
@@ -121,6 +146,32 @@ pub async fn get_calculation_data(
     Ok(data)
 }
 
+fn unwrap_data_pair(
+    data1: Option<&CalculationsDatum>,
+    data2: Option<&CalculationsDatum>,
+) -> Result<Option<(f64, f64)>, Error> {
+    // deal with unwrapping the options, choosing correct if exists, or else original
+    match (data1, data2) {
+        (Some(data1), Some(data2)) => {
+            match (
+                data1.corrected,
+                data1.original,
+                data2.corrected,
+                data2.original,
+            ) {
+                (Some(data1_corr), Some(_), Some(data2_corr), Some(_)) => {
+                    Ok(Some((data1_corr, data2_corr)))
+                }
+                (None, Some(data1_orig), None, Some(data2_orig)) => {
+                    Ok(Some((data1_orig, data2_orig)))
+                }
+                _ => Ok(None),
+            }
+        }
+        _ => Ok(None),
+    }
+}
+
 pub async fn products_available_handler(
     Path(element_id): Path<String>,
     State(product_tables): State<ProductTables>,
@@ -164,47 +215,25 @@ pub async fn products_handler(
         // do the calculation
         for (time, vector) in d {
             // if have the same timestamp for the input paramids
-            if element_id == "dew_point_temperature" {
-                // dew point temperature
-                // need temperature and relative humidity
-
-                let find_air_temperature = vector.iter().find(|v| v.paramid == 211);
-                let find_relative_humidity = vector.iter().find(|v| v.paramid == 262);
-                match (find_air_temperature, find_relative_humidity) {
-                    (Some(found_air_temperature), Some(found_relative_humidity)) => {
-                        match (
-                            found_air_temperature.corrected,
-                            found_air_temperature.original,
-                            found_relative_humidity.corrected,
-                            found_relative_humidity.original,
-                        ) {
-                            (Some(air_temperature), Some(_), Some(relative_humidity), Some(_)) => {
-                                let value =
-                                    dew_point_temperature(air_temperature, relative_humidity)
-                                        .unwrap();
-                                response.push(ProductsResponse {
-                                    name: element_id.clone(),
-                                    timestamp: time,
-                                    value,
-                                });
-                            }
-                            (None, Some(air_temperature), None, Some(relative_humidity)) => {
-                                let value =
-                                    dew_point_temperature(air_temperature, relative_humidity)
-                                        .unwrap();
-                                response.push(ProductsResponse {
-                                    name: element_id.clone(),
-                                    timestamp: time,
-                                    value,
-                                });
-                            }
-                            _ => println!(
-                                "Didn't have either both corrected values, or both original"
-                            ),
-                        };
+            match element_id.as_str() {
+                // TODO: make a massive match statement with all the names???
+                "dew_point_temperature" => {
+                    let find_air_temperature = vector.iter().find(|v| v.paramid == 211);
+                    let find_relative_humidity = vector.iter().find(|v| v.paramid == 262);
+                    // see if we have the two values
+                    let data_pair = unwrap_data_pair(find_air_temperature, find_relative_humidity)
+                        .map_err(error::internal_error)?;
+                    if let Some((air_temperature, relative_humidity)) = data_pair {
+                        let value =
+                            dew_point_temperature(air_temperature, relative_humidity).unwrap();
+                        response.push(ProductsResponse {
+                            name: element_id.clone(),
+                            timestamp: time,
+                            value,
+                        });
                     }
-                    _ => println!("Didn't find both paramids"),
                 }
+                _ => error!("No calculations match for element_id: {}", element_id),
             }
         }
     }
