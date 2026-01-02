d9c42324023c1dff10799e84b7d3ff242599d63a
Louise Carolyn Oram <louiseo@met.no>
2026-01-02 13:53:43 +0100

    return 404 when cannot find file from s3 instead of internal error

--- integration_tests/tests/dut_municipality.rs
+++ integration_tests/tests/dut_municipality.rs
@@ -69,8 +69,7 @@ async fn test_dut_municipality_read_file() {
                     assert_eq!(json, expected, "{case}");
                 }
                 None => {
-                    // TODO: this should probably return a 404
-                    assert!(resp.status().is_server_error(), "{case}")
+                    assert!(resp.status().is_client_error(), "{case}")
                 }
             }
         }
