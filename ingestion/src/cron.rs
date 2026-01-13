742903f0ebdf5ad6434897a787228355db03a8cb
Louise Carolyn Oram <louiseo@met.no>
2026-01-13 13:39:50 +0100

    if the max/min query fails, then use empty timerange

--- ingestion/src/cron.rs
+++ ingestion/src/cron.rs
@@ -39,12 +39,15 @@ pub async fn refresh_from_to((stinfosys, pools): &(Stinfosys, DbPools)) {
     let mut open_conn = pools.open.get().await.unwrap();
     let mut restricted_conn = pools.restricted.get().await.unwrap();
 
+    info!("Caching closed stations and observation programs from StInfoSys");
     let (obs_pgm_times_map, station_times_map) = stinfosys.cache_closed_stinfosys().await.unwrap();
 
+    info!("Updating open and restricted database timeseries");
     let (open_res, restricted_res) = tokio::join!(
         tsupdate::update_from_to(&mut open_conn, &obs_pgm_times_map, &station_times_map),
         tsupdate::update_from_to(&mut restricted_conn, &obs_pgm_times_map, &station_times_map),
     );
+    info!("Finished updating open and restricted database timeseries");
 
     if let Err(err) = open_res {
         error!("Error while updating open db timeseries: {err}");
