8cf1e08951bb1d58f4e7961ebd2f84bc76e6c204
Louise Carolyn Oram <louiseo@met.no>
2026-02-12 21:31:53 +0100

    add useinfo and controlinfo to patchwork datum

--- egress/src/patchwork.rs
+++ egress/src/patchwork.rs
@@ -149,10 +149,12 @@ pub struct PatchworkDatum {
     // can assume have original and timestamp? (the field for original value
     // can technically be null (database schema wise)...)
     // definitely do not always have corrected and quality code (eg. before qc'ing)
-    original: Option<f64>,
     timestamp: DateTime<Utc>,
+    original: Option<f64>,
     corrected: Option<f64>,
     quality_code: Option<i32>,
+    control_info: Option<String>,
+    use_info: Option<String>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
@@ -615,7 +617,7 @@ pub async fn get_patchwork(
 
     let query = conn
         .prepare(
-            "SELECT timeseries, obstime, original, corrected, quality_code \
+            "SELECT timeseries, obstime, original, corrected, quality_code, controlinfo, useinfo \
             FROM legacy.data \
             WHERE timeseries = $1 \
                 AND obstime >= $2 \
@@ -647,10 +649,12 @@ pub async fn get_patchwork(
         };
         for row in rows {
             data.push(PatchworkDatum {
-                original: row.get(2),
                 timestamp: row.get(1),
+                original: row.get(2),
                 corrected: row.get(3),
                 quality_code: row.get(4),
+                control_info: row.get(5),
+                use_info: row.get(6),
             });
         }
     }
