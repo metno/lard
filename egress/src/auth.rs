c11a4e5c7cd8b7c77a5b72218db17e9167f06619
Louise Carolyn Oram <louiseo@met.no>
2026-03-11 13:46:32 +0100

    move checking the outcome of parsing the token to the middleware

--- egress/src/auth.rs
+++ egress/src/auth.rs
@@ -96,10 +96,7 @@ fn parse_stations(roles: Vec<String>) -> Vec<i32> {
 pub fn verify_token(token_str: &str, certs: JWKScerts) -> Result<(Vec<i32>, Vec<i32>), Error> {
     let mut validation = Validation::new(Algorithm::ES384);
     validation.set_audience(&["ODA"]);
-    let token_message = match decode::<Claims>(token_str, &certs, &validation) {
-        Ok(message) => message,
-        Err(_) => return Ok((vec![], vec![])), // if the token is invalid, return empty scopes, this user has only open data access
-    };
+    let token_message = decode::<Claims>(token_str, &certs, &validation)?;
 
     Ok((
         parse_permitid(token_message.claims.resource_access.resource.roles.clone()),
@@ -126,8 +123,10 @@ pub async fn auth_middleware(
         .and_then(parse_auth_header)
     {
         Some(token) => {
-            // if errors, the token is invalid
-            let roles = verify_token(&token, certs).map_err(error::unauthorized)?;
+            // if errors, then default to open access (aka empty roles)
+            let roles = verify_token(&token, certs)
+                .map_err(error::unauthorized)
+                .unwrap_or_default();
             req.extensions_mut().insert(Some(roles));
         }
         None => {
