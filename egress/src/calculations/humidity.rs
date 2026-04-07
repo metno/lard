211dd6aa39c3f3c7fc458b76b4b7960e7803778f
Louise Carolyn Oram <louiseo@met.no>
2026-04-07 14:16:12 +0200

    humidity doc comments, and create some const variables

--- egress/src/calculations/humidity.rs
+++ egress/src/calculations/humidity.rs
@@ -1,106 +1,104 @@
-// calculations related to humidity products
+// Calculations related to humidity products
+// Define some constansts that are used in the calculations of humidity-related products.
+const C: f64 = 6.10780; // [hPa]
+const A_POS: f64 = 17.08085; // [rate] or [deg.C / deg.C]
+const A_NEG: f64 = 17.84362; // [rate] or [deg.C / deg.C]
+const B_POS: f64 = 234.175; // [deg.C]
+const B_NEG: f64 = 245.425; // [deg.C]
 
+/// ref.: The Relationship between Relative Humidity and the Dewpoint Temperature in Moist Air:
+/// A Simple Conversion and Applications (2005) - https://doi.org/10.1175/BAMS-86-2-225
+/// Equation (6) : e_s = c * exp( (a * ta) / (b + ta) )
+/// e_s = saturation_vapor_pressure [hPa]
+/// ta = air_temperature [deg.C]
+/// 'Commonly known as the Magnus formula (empirical) although a rather inaccurate attribution'
+/// extra info:  https://en.wikipedia.org/wiki/Vapor_pressure_of_water
+/// Water vapor pressure behaves non-linearly with temperature:
+/// - At higher temperatures, water molecules have more kinetic energy, leading to a rapid increase in vapor pressure.
+/// - At lower temperatures (especially below freezing), the relationship changes due to differences in the behavior of ice versus liquid water.
+///
+/// Approximations: Tetens or August-Roche-Magnus equation... the form is the same, but coefficients differ based on temperature range.
+///
+/// [IMPORTANT] Here we use the following coefficients:
+/// for ta >  0, a = 17.08085 [rate] or [deg.C / deg.C], b = 234.175 [deg.C], and c = 6.10780 [hPa]. DEW POINT
+/// for ta <= 0, a = 17.84362 [rate] or [deg.C / deg.C], b = 245.425 [deg.C], and c = 6.10780 [hPa]. FROST POINT
 fn calculate_saturation_vapor_pressure(air_temperature: f64) -> f64 {
-    // ref.: The Relationship between Relative Humidity and the Dewpoint Temperature in Moist Air:
-    // A Simple Conversion and Applications (2005) - https://doi.org/10.1175/BAMS-86-2-225
-    // Equation (6) : e_s = c * exp( (a * ta) / (b + ta) )
-    // e_s = saturation_vapor_pressure [hPa]
-    // ta = air_temperature [deg.C]
-    // 'Commonly known as the Magnus formula (empirical) although a rather inaccurate attribution'
-    //
-    // extra info:  https://en.wikipedia.org/wiki/Vapor_pressure_of_water
-    // Water vapor pressure behaves non-linearly with temperature:
-    // - At higher temperatures, water molecules have more kinetic energy, leading to a rapid increase in vapor pressure.
-    // - At lower temperatures (especially below freezing), the relationship changes due to differences in the behavior of ice versus liquid water.
-    // Approxmations: Tetens or Aguste-Roche-Magnus equation... the form is the same, but coefficients differ based on temperature range.
-    //
-    // [IMPORTANT] Here we use the following coefficients:
-    // for ta >  0, a = 17.08085 [rate] or [deg.C / deg.C], b = 234.175 [deg.C], and c = 6.10780 [hPa]. DEW POINT
-    // for ta <= 0, a = 17.84362 [rate] or [deg.C / deg.C], b = 245.425 [deg.C], and c = 6.10780 [hPa]. FROST POINT
-
     if air_temperature <= 0.0 {
-        return 6.10780 * (17.84362 * air_temperature / (245.425 + air_temperature)).exp();
+        return C * (A_NEG * air_temperature / (B_NEG + air_temperature)).exp();
     }
-    6.10780 * (17.08085 * air_temperature / (234.175 + air_temperature)).exp()
+    C * (A_POS * air_temperature / (B_POS + air_temperature)).exp()
 }
 
+/// ref.: The Relationship between Relative Humidity and the Dewpoint Temperature in Moist Air:
+/// A Simple Conversion and Applications (2005) - https://doi.org/10.1175/BAMS-86-2-225
+/// Equation (3) : RH = 100 * e / e_s  -or-  e = (RH * e_s) / 100
+/// 'RH' Relative humidity is commonly defined as the ratio of
+/// 'e' the actual water vapor pressure [hPa] to
+/// 'e_s' the 'saturation' vapor pressure [hPa]
+/// 1 hPa = 1 mb = 100 Pascals are units commonly used in meteorology and atmospheric sciences.
 pub fn calculate_water_vapor_partial_pressure(
     saturation_vapor_pressure: f64,
     relative_humidity: f64,
 ) -> f64 {
-    // ref.: The Relationship between Relative Humidity and the Dewpoint Temperature in Moist Air:
-    // A Simple Conversion and Applications (2005) - https://doi.org/10.1175/BAMS-86-2-225
-    // Equation (3) : RH = 100 * e / e_s  -or-  e = (RH * e_s) / 100
-    // 'RH' Relative humidity is commonly defined as the ratio of
-    // 'e' the actual water vapor pressure [hPa] to
-    // 'e_s' the 'saturation' vapor pressure [hPa]
-    // 1 hPa = 1 mb = 100 Pascals are units commonly used in meteorology and atmospheric sciences.
-
     (relative_humidity * saturation_vapor_pressure) / 100.0
 }
 
+/// ref.: The Relationship between Relative Humidity and the Dewpoint Temperature in Moist Air:
+/// A Simple Conversion and Applications (2005) - https://doi.org/10.1175/BAMS-86-2-225
+/// Equation (7) : td = ( b * ln( e / c ) ) / ( a - ln( e / c ) )
+/// td = dew_point_temperature [deg.C]
+/// e = water_vapor_partial_pressure [hPa]
+/// ekstra info: Guide to Instruments and Methods of Observation (WMO-No. 8)
+/// Chapter 4: Humidity - Annex 4.B: Formulae for the computation of measures of humidity
 pub fn calculate_dew_point_temperature(
     water_vapor_partial_pressure: f64,
     air_temperature: f64,
 ) -> f64 {
-    // ref.: The Relationship between Relative Humidity and the Dewpoint Temperature in Moist Air:
-    // A Simple Conversion and Applications (2005) - https://doi.org/10.1175/BAMS-86-2-225
-    // Equation (7) : td = ( b * ln( e / c ) ) / ( a - ln( e / c ) )
-    // td = dew_point_temperature [deg.C]
-    // e = water_vapor_partial_pressure [hPa]
-    //
-    // ekstra info: Guide to Instruments and Methods of Observation (WMO-No. 8)
-    // Chapter 4: Humidity - Annex 4.B: Formulae for the computation of measures of humidity
-    //
-    // Should we set a condition that dew_point_temperature cannot be greater than air_temperature???
-
+    // TODO: Should we set a condition that dew_point_temperature cannot be greater than air_temperature???
     if air_temperature <= 0.0 {
-        return 245.425 * (water_vapor_partial_pressure / 6.10780).ln()
-            / (17.84362 - (water_vapor_partial_pressure / 6.10780).ln());
+        return B_NEG * (water_vapor_partial_pressure / C).ln()
+            / (A_NEG - (water_vapor_partial_pressure / C).ln());
     }
-    234.175 * (water_vapor_partial_pressure / 6.10780).ln()
-        / (17.08085 - (water_vapor_partial_pressure / 6.10780).ln())
+    B_POS * (water_vapor_partial_pressure / C).ln()
+        / (A_POS - (water_vapor_partial_pressure / C).ln())
 }
 
+/// ref.: Guide to Instruments and Methods of Observation (WMO-No. 8) - Chapter 4: Humidity
+/// Annex 4.A: Definitions and specifications of water vapor in the atmosphere
+/// Equation (4.A.1) : r = m_v / m_a
+/// mixing ratio 'r' is defined as the mass 'm_v' of water vapor per unit mass 'm_a' of dry air in g/kg or kg/kg
+/// Here, the mixing ratio is DERIVED FROM Equation (4.A.6) : e = po * r / ( epsilon + r ), then giving:
+/// Equation: r = epsilon * ( e / (po - e) )
+/// r: humidity_mixing_ratio [kg/kg], - see conversion note below
+/// e: water_vapor_partial_pressure [hPa],
+/// po: surface_air_pressure [hPa],
+/// epsilon: ratio of the molecular weight of water vapor to dry air, approximately 0.62198 [dimensionless] or [g/mol / g/mol].
+///
+/// IMPORTANT!! Add a CONVERSION from [kg/kg] to [g/kg] by multiplying by 1000 to match units for 'r' in stinfosys i.e. [g/kg].
 pub fn calculate_humidity_mixing_ratio(
     water_vapor_partial_pressure: f64,
     surface_air_pressure: f64,
 ) -> f64 {
-    // ref.: Guide to Instruments and Methods of Observation (WMO-No. 8) - Chapter 4: Humidity
-    // Annex 4.A: Definitions and specifications of water vapor in the atmosphere
-    // Equation (4.A.1) : r = m_v / m_a
-    // mixing ratio 'r' is defined as the mass 'm_v' of water vapor per unit mass 'm_a' of dry air in g/kg or kg/kg
-    //
-    // Here, the mixing ratio is DERIVED FROM Equation (4.A.6) : e = po * r / ( epsilon + r ), then giving:
-    // Equation: r = epsilon * ( e / (po - e) )
-    // r: humidity_mixing_ratio [kg/kg], - see conversion note below
-    // e: water_vapor_partial_pressure [hPa],
-    // po: surface_air_pressure [hPa],
-    // epsilon: ratio of the molecular weight of water vapor to dry air, approximately 0.62198 [dimensionless] or [g/mol / g/mol].
-    //
-    // IMPORTANT!! Add a CONVERSION from [kg/kg] to [g/kg] by multiplying by 1000 to match units for 'r' in stinfosys i.e. [g/kg].
-
     1000.0 * 0.62198 * water_vapor_partial_pressure
         / (surface_air_pressure - water_vapor_partial_pressure)
 }
 
+/// ref.: Guide to Instruments and Methods of Observation (WMO-No. 8) - Chapter 4: Humidity
+/// Annex 4.A: Definitions and specifications of water vapor in the atmosphere
+/// Equation (4.A.2) : q = m_v / ( m_a + m_v )
+/// specific humidity 'q' is defined as the mass 'm_v' of water vapor per unit mass of moist air in g/kg or kg/kg
+///
+/// Here, we subsitute 'm_v' with the mixing ratio 'r' from Equation (4.A.1) : m_v = r * m_a in Equation (4.A.2), then giving:
+/// Equation: q = r / (1 + r) - !!NOT USED AS SUCH HERE!!
+/// 'r' the actual water vapor dry mass mixing ratio [kg/kg] - see conversion note below
+/// 'q' specific humidity [kg/kg] - see conversion note below
+///
+/// IMPORTANT!! Here, the function is adapted for [g/kg] units to match units for 'r' and 'q' in stinfosys i.e. [g/kg].
+/// Equation: q = (r / 1000) / (1 + (r / 1000)), giving
+/// FINAL Equation: q = r / (1000 + r)
+/// 'r' the actual water vapor dry mass mixing ratio [g/kg]
+/// 'q' specific humidity [g/kg]
 pub fn calculate_specific_humidity(humidity_mixing_ratio: f64) -> f64 {
-    // ref.: Guide to Instruments and Methods of Observation (WMO-No. 8) - Chapter 4: Humidity
-    // Annex 4.A: Definitions and specifications of water vapor in the atmosphere
-    // Equation (4.A.2) : q = m_v / ( m_a + m_v )
-    // specific humidity 'q' is defined as the mass 'm_v' of water vapor per unit mass of moist air in g/kg or kg/kg
-    //
-    // Here, we subsitute 'm_v' with the mixing ratio 'r' from Equation (4.A.1) : m_v = r * m_a in Equation (4.A.2), then giving:
-    // Equation: q = r / (1 + r) - !!NOT USED AS SUCH HERE!!
-    // 'r' the actual water vapor dry mass mixing ratio [kg/kg] - see conversion note below
-    // 'q' specific humidity [kg/kg] - see conversion note below
-    //
-    // IMPORTANT!! Here, the function is adapted for [g/kg] units to match units for 'r' and 'q' in stinfosys i.e. [g/kg].
-    // Equation: q = (r / 1000) / (1 + (r / 1000)), giving
-    // FINAL Equation: q = r / (1000 + r)
-    // 'r' the actual water vapor dry mass mixing ratio [g/kg]
-    // 'q' specific humidity [g/kg]
-
     humidity_mixing_ratio / (1000.0 + humidity_mixing_ratio)
 }
 
