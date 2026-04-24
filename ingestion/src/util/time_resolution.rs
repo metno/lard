5b0043378f671b28200146654d95a92b03f86f4a
Louise Carolyn Oram <louiseo@met.no>
2026-04-24 13:23:46 +0200

    refactor and fix bug

--- ingestion/src/util/time_resolution.rs
+++ ingestion/src/util/time_resolution.rs
@@ -40,18 +40,23 @@ pub async fn find_timeresolution_of_timeseries(
             ORDER BY frequency DESC
             LIMIT 3;", &[&ts]).await?;
 
-    if resolution_results.len() == 1 {
+    if resolution_results.is_empty() {
+        // Don't know the time resolution
+        Err(Error::Timeresolution("unknown".to_string()))
+    } else if resolution_results.len() == 1 {
         // There is only one resolution, so its the timeresolution of the timeseries
         let resolution: Interval = resolution_results[0].get("resolution");
         Ok(resolution)
-    } else if resolution_results.len() <= 2 {
+    } else {
+        // at least 2, possibly a 3rd (since Limit 3)
         let resolution1: Interval = resolution_results[0].get("resolution");
         let resolution2: Interval = resolution_results[1].get("resolution");
         let frequency1: i64 = resolution_results[0].get("frequency");
         let frequency2: i64 = resolution_results[1].get("frequency");
         let frequency3: Option<i64> = resolution_results.get(2).map(|row| row.get("frequency"));
 
-        // If the most common resolution is at least 100 times more frequent than the second and third most common, we consider it the main resolution
+        // If the most common resolution is at least 100 times more frequent than the second and third most common,
+        // we consider it the main resolution
         if frequency1 >= 100 * (frequency2 + frequency3.unwrap_or(0)) {
             Ok(resolution1)
         } else {
@@ -65,10 +70,33 @@ pub async fn find_timeresolution_of_timeseries(
                     ),
             ))
         }
-    } else {
-        // Else we don't know the timeresolution
-        Err(Error::Timeresolution("unknown".to_string()))
     }
+}
+
+async fn refresh_timeresolutions(
+    conn: &PooledPgConn<'_>,
+) -> Result<(std::collections::HashMap<i64, String>, i32), Error> {
+    let timeseries_rows = conn.query(ALL_TIMESERIES_QUERY, &[]).await?;
+    // keep a hashmap of the issues we encounter, so we can log them at the end of the process
+    let mut timeresolution_issues: std::collections::HashMap<i64, String> =
+        std::collections::HashMap::new();
+    let mut count = 0;
+
+    for x in timeseries_rows {
+        let ts_id: i64 = x.get("id");
+        let timeresolution = find_timeresolution_of_timeseries(conn, ts_id).await;
+        if let Ok(timeresolution) = timeresolution {
+            conn.execute(SET_TIMERESOLUTION_QUERY, &[&timeresolution, &ts_id])
+                .await?;
+            count += 1;
+        } else {
+            warn!(
+                "Failed to find timeresolution for timeseries {ts_id} in open db: {timeresolution:?}"
+            );
+            timeresolution_issues.insert(ts_id, format!("{timeresolution:?}"));
+        }
+    }
+    Ok((timeresolution_issues, count))
 }
 
 pub async fn refresh_timeresolution_repeatedly(
@@ -84,47 +112,24 @@ pub async fn refresh_timeresolution_repeatedly(
             _ = refresh_interval.tick() => {
 
                 info!("Updating timeresolution...");
-                // keep a hashmap of the issues we encounter, so we can log them at the end of the process
-                let mut timeresolution_issues: std::collections::HashMap<i64, String> = std::collections::HashMap::new();
-                let mut count = 0;
 
                 let _ = async {
                     let open_conn = pools.open.get().await?;
                     let restricted_conn = pools.restricted.get().await?;
-                    let open_timeseries_rows = open_conn.query(ALL_TIMESERIES_QUERY, &[]).await?;
-                    for x in open_timeseries_rows {
-                        let ts_id: i64 = x.get("id");
-                        let timeresolution = find_timeresolution_of_timeseries(&open_conn, ts_id).await;
-                        if let Ok(timeresolution) = timeresolution {
-                            open_conn.execute(SET_TIMERESOLUTION_QUERY, &[&timeresolution, &ts_id]).await?;
-                            count += 1;
-                        } else {
-                            warn!("Failed to find timeresolution for timeseries {ts_id} in open db: {timeresolution:?}");
-                            timeresolution_issues.insert(ts_id, format!("{timeresolution:?}"));
-                        }
-                    }
+                    // update open
+                    let (open_timeresolution_issues, open_count) = refresh_timeresolutions(&open_conn).await?;
                     info!("Finished updating timeresolution in open db");
+                    info!("Updated timeresolution for {open_count} timeseries in open db, failed to find timeresolution for {} timeseries", open_timeresolution_issues.len());
+                    info!("Issues encountered for timeseries in open db: {:?}", open_timeresolution_issues);
 
-                    let restricted_timeseries_rows = restricted_conn.query(ALL_TIMESERIES_QUERY, &[]).await?;
-                    for x in restricted_timeseries_rows {
-                        let ts_id: i64 = x.get("id");
-                        let timeresolution = find_timeresolution_of_timeseries(&restricted_conn, ts_id).await;
-                        if let Ok(timeresolution) = timeresolution {
-                            restricted_conn.execute(SET_TIMERESOLUTION_QUERY, &[&timeresolution, &ts_id]).await?;
-                            count += 1;
-                        } else {
-                            warn!("Failed to find timeresolution for timeseries {ts_id} in restricted db: {timeresolution:?}");
-                            timeresolution_issues.insert(ts_id, format!("{timeresolution:?}"));
-                        }
-                    }
+                    // update restricted
+                    let (restricted_timeresolution_issues, restricted_count) = refresh_timeresolutions(&restricted_conn).await?;
                     info!("Finished updating timeresolution in restricted db");
+                    info!("Updated timeresolution for {restricted_count} timeseries in restricted db, failed to find timeresolution for {} timeseries", restricted_timeresolution_issues.len());
+                    info!("Issues encountered for timeseries in restricted db: {:?}", restricted_timeresolution_issues);
 
                     Ok::<(), Error>(())
                 }.await.inspect_err(|err| warn!("failed to refresh timeresolution: {err}"));
-
-                // print out some useful statistics
-                info!("Updated timeresolution for {count} timeseries, failed to find timeresolution for {} timeseries", timeresolution_issues.len());
-                info!("Issues encountered for timeseries: {:?}", timeresolution_issues);
             }
         }
     }
