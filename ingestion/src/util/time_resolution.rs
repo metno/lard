2243ff6fb4780b3bd8cc9f23c82d910e86e13a11
Louise Carolyn Oram <louiseo@met.no>
2026-05-11 22:23:32 +0200

    compare recent resolution to overall resolution

--- ingestion/src/util/time_resolution.rs
+++ ingestion/src/util/time_resolution.rs
@@ -15,16 +15,21 @@ const SET_TIMERESOLUTION_QUERY: &str = "\
     SET timeresolution = $1 \
     WHERE id = $2";
 
-pub async fn find_timeresolution_of_timeseries(
+pub async fn find_time_resolution_of_timeseries_recent_or_all(
     conn: &PooledPgConn<'_>,
     ts: i64,
-) -> Result<Interval, Error> {
-    let resolution_results = conn.query("WITH data AS (                                                                                                                                                 
+    last_obstime: Option<chrono::NaiveDateTime>,
+) -> Result<Vec<(Interval, i64)>, Error> {
+    let resolution_results = if let Some(last_obstime) = last_obstime {
+        // query with time filter, so we only look at the last 7 days of data
+        // TODO: determine if 7 days is a good time range?
+        conn.query("WITH data AS (                                                                                                                                                 
                 SELECT
                     date_trunc('minute', obstime + interval '30 second') as obs_time,
                     LAG(date_trunc('minute', obstime + interval '30 second')) OVER (ORDER BY obstime) as prev_obs_time
                 FROM legacy.data
                 WHERE timeseries=$1
+                AND obstime >= $2 - interval '7 day'
             ),
             gaps AS (
                 SELECT
@@ -38,35 +43,123 @@ pub async fn find_timeresolution_of_timeseries(
             FROM gaps
             GROUP BY resolution
             ORDER BY frequency DESC
-            LIMIT 3;", &[&ts]).await?;
-
-    if resolution_results.is_empty() {
-        // Don't know the time resolution
-        Err(Error::Timeresolution("unknown".to_string()))
-    } else if resolution_results.len() == 1 {
-        // There is only one resolution, so its the timeresolution of the timeseries
-        let resolution: Interval = resolution_results[0].get("resolution");
-        Ok(resolution)
+            LIMIT 3;", &[&ts, &last_obstime]).await?
     } else {
-        // at least 2, possibly a 3rd (since Limit 3)
-        let resolution1: Interval = resolution_results[0].get("resolution");
-        let resolution2: Interval = resolution_results[1].get("resolution");
-        let frequency1: i64 = resolution_results[0].get("frequency");
-        let frequency2: i64 = resolution_results[1].get("frequency");
-        let frequency3: Option<i64> = resolution_results.get(2).map(|row| row.get("frequency"));
+        // query without time filter, so we look at all data
+        // TODO: we also want to look at the offset?
+        conn.query("WITH data AS (                                                                                                                                                 
+                    SELECT
+                        date_trunc('minute', obstime + interval '30 second') as obs_time,
+                        LAG(date_trunc('minute', obstime + interval '30 second')) OVER (ORDER BY obstime) as prev_obs_time
+                    FROM legacy.data
+                    WHERE timeseries=$1
+                ),
+                gaps AS (
+                    SELECT
+                        (obs_time - prev_obs_time) as resolution
+                    FROM data
+                    WHERE prev_obs_time IS NOT NULL
+                )
+                SELECT
+                    resolution,
+                    COUNT(*) as frequency
+                FROM gaps
+                GROUP BY resolution
+                ORDER BY frequency DESC
+                LIMIT 3;", &[&ts]).await?
+    };
 
+    let resolutions = resolution_results
+        .iter()
+        .map(|row| {
+            let resolution: Interval = row.get("resolution");
+            let frequency: i64 = row.get("frequency");
+            (resolution, frequency)
+        })
+        .collect();
+    Ok(resolutions)
+}
+
+async fn last_obstime_ts(
+    conn: &PooledPgConn<'_>,
+    ts: i64,
+) -> Result<Option<chrono::NaiveDateTime>, Error> {
+    let possible_last_obstime = conn
+        .query(
+            "SELECT MAX(obstime) \
+                    FROM legacy.data \
+                    WHERE timeseries = $1",
+            &[&ts],
+        )
+        .await?;
+    if let Some(row) = possible_last_obstime.first() {
+        let last_obstime: Option<chrono::NaiveDateTime> = row.get(0);
+        Ok(last_obstime)
+    } else {
+        Ok(None)
+    }
+}
+
+pub async fn determine_time_resolution_of_timeseries(
+    conn: &PooledPgConn<'_>,
+    ts: i64,
+) -> Result<Interval, Error> {
+    let last_obstime = last_obstime_ts(conn, ts).await?;
+    let resolution_results_recent = if let Some(last_obstime) = last_obstime {
+        find_time_resolution_of_timeseries_recent_or_all(conn, ts, Some(last_obstime)).await?
+    } else {
+        // skip this timeseries, it appears to have no data?
+        return Err(Error::Timeresolution("no data".to_string()));
+    };
+    let resolution_results_all =
+        find_time_resolution_of_timeseries_recent_or_all(conn, ts, None).await?;
+
+    if resolution_results_all.is_empty() {
+        // Don't know the time resolution, this probably shouldn't happen
+        // since we already checked there is data but just in case, we can return unknown
+        Err(Error::Timeresolution("unknown".to_string()))
+    } else {
+        // at least 1, possibly a 2 or 3 (since Limit 3)
+        let resolution1: Interval = resolution_results_all[0].0;
+        let resolution2: Option<Interval> = resolution_results_all.get(1).map(|row| row.0);
+
+        // check the main resolution is the same in the recent and all data, if not ... its problematic
+        // maybe the timeseries needs to be broken up?
+        if resolution_results_recent.is_empty() {
+            // if there is no data near the last obstime (this is unlikely to happen?)
+            // TODO: report in for problem collection?
+            return Err(Error::Timeresolution("unknown".to_string()));
+        } else if resolution1 != resolution_results_recent[0].0 {
+            // if the most common resolution in the recent data is different from the most common resolution in all data, we have a problem
+            return Err(Error::Timeresolution(
+                "inconsistent".to_string()
+                    + &format!(
+                        " (most common resolution in recent data: {})",
+                        resolution_results_recent[0].0.to_iso_8601()
+                            + &format!(
+                                " (most common resolution in all data: {})",
+                                resolution1.to_iso_8601()
+                            ),
+                    ),
+            ));
+        }
+        let frequency1: i64 = resolution_results_all[0].1;
+        let frequency2: Option<i64> = resolution_results_all.get(1).map(|row| row.1);
+        let frequency3: Option<i64> = resolution_results_all.get(2).map(|row| row.1);
         // If the most common resolution is at least 100 times more frequent than the second and third most common,
         // we consider it the main resolution
-        if frequency1 >= 100 * (frequency2 + frequency3.unwrap_or(0)) {
+        if frequency1 >= 100 * (frequency2.unwrap_or(0) + frequency3.unwrap_or(0)) {
+            // we also know at this point that it agrees with the resolution in the recent data
+            // TODO: check for offset here???
             Ok(resolution1)
         } else {
             // could just say its unknown, do we want to differentiate?
             Err(Error::Timeresolution(
                 "irregular".to_string()
                     + &format!(
-                        " (top 2 resolutions: {} {})",
+                        " (top 2 resolutions in all data: {} {})",
                         resolution1.to_iso_8601(),
-                        resolution2.to_iso_8601()
+                        resolution2.map_or("unknown".to_string(), |r| r.to_iso_8601())
                     ),
             ))
         }
@@ -84,14 +177,14 @@ async fn refresh_timeresolutions(
 
     for x in timeseries_rows {
         let ts_id: i64 = x.get("id");
-        let timeresolution = find_timeresolution_of_timeseries(conn, ts_id).await;
+        let timeresolution = determine_time_resolution_of_timeseries(conn, ts_id).await;
         if let Ok(timeresolution) = timeresolution {
             conn.execute(SET_TIMERESOLUTION_QUERY, &[&timeresolution, &ts_id])
                 .await?;
             count += 1;
         } else {
             warn!(
-                "Failed to find timeresolution for timeseries {ts_id} in open db: {timeresolution:?}"
+                "Failed to find timeresolution for timeseries {ts_id}, error message: {timeresolution:?}"
             );
             timeresolution_issues.insert(ts_id, format!("{timeresolution:?}"));
         }
